24e3a6792f2e2844bb12e576bfe44edab514a447
Clay Didier <csdidier@gmail.com>
2010-01-27 16:34:49 +0000

    (original rev 324) Fixed style in Debug files (8-space tab, etc. . . )
    git-svn-id: file:///tmp/current/omni/trunk@117 2ffc8166-1744-45da-a82a-d6ea10f7896b

--- omni/src/system/omDebug.cpp
+++ omni/src/system/omDebug.cpp
@@ -7,6 +7,10 @@
 
 //***********************************************************************
 // TODO Convert to command line switches ---> get rid of #defs
+//      - Make this a singleton class
+//      - Include a ParseArg function
+//      - Include a Default Settings Section
+//      - Command Line input will override default
 //***********************************************************************
 
 
@@ -14,7 +18,6 @@
 //***********************************************************************
 //****  SET YOUR DEF FLAGS HERE  (temporary)               
 //***********************************************************************
-
 #define OM_CACHE           0        // Testing of the Cache
 #define OM_TESTRUN         0        // Gen One's Old Debugs
 #define OM_MUTEX           0        // pthread_mutexes - all mutexes noted
@@ -27,52 +30,52 @@
 
 
 
-// Your general debug function . . . it prints
+/** Your general debug function . . . it prints to stdout */
 void debug(const char *category,const char *format, ...)
 {
         va_list args;
         va_start(args, format);
 
 //----------------------------------------------	
-	if (!strcmp(category,"cache")){
+        if (!strcmp(category,"cache")){
 #if OM_CACHE
                 vfprintf(stdout,format,args);		        
 #endif
-	}
+	   }
 //----------------------------------------------
-	  if (!strcmp(category,"testrun")){
+	   if (!strcmp(category,"testrun")){
 #if OM_TESTRUN
-	    vfprintf(stdout,format,args);
+                vfprintf(stdout,format,args);
 #endif
-	}
+	   }
 //----------------------------------------------
-	if (!strcmp(category,"mesher")){
+	   if (!strcmp(category,"mesher")){
 #if OM_MESHER
                 vfprintf(stdout,format,args);		        
 #endif
-	}
-	va_end(args);
+	   }
+	   va_end(args);
 //----------------------------------------------
-	if (!strcmp(category,"genone")){
+	   if (!strcmp(category,"genone")){
 #if OM_GENONE
                 vfprintf(stdout,format,args);		        
 #endif
-	}
+	   }
 //----------------------------------------------
-	if (!strcmp(category,"mutex")){
+	   if (!strcmp(category,"mutex")){
 #if OM_MUTEX
-           vfprintf(stdout,format,args);		        
+                vfprintf(stdout,format,args);		        
 #endif
-	}
+	   }
 //----------------------------------------------
-	if (!strcmp(category,"thread")){
+	   if (!strcmp(category,"thread")){
 #if OM_THREAD
-           vfprintf(stdout,format,args);		        
+                vfprintf(stdout,format,args);		        
 #endif
-	}
+	   }
 //----------------------------------------------
 
-	va_end(args);
+	   va_end(args);
 }
 
 
--- omni/src/system/omDebug.h
+++ omni/src/system/omDebug.h
@@ -5,15 +5,12 @@
 void debug(const char *category,const char *format, ...);
 
 #define pthread_mutex_lock(x) debug("mutex","locking enter: %p (line:fun) %i:%s \n",x,__LINE__, __FUNCTION__);\
-pthread_mutex_lock(x);\
-debug("mutex","locking exit: %p (line:fun) %i:%s \n",x,__LINE__, __FUNCTION__);
+        pthread_mutex_lock(x);                                             \
+        debug("mutex","locking exit: %p (line:fun) %i:%s \n",x,__LINE__, __FUNCTION__);
 
 #define pthread_mutex_unlock(x) debug("mutex","unlocking enter: %p (line:fun) %i:%s \n",x,__LINE__, __FUNCTION__);\
-pthread_mutex_unlock(x);\
-debug("mutex","unlocking exit: %p (line:fun) %i:%s \n",x,__LINE__, __FUNCTION__);
-
-//#define pthread_create(w,x,y,z)   pthread_create(w,x,y,z);            \
-//debug("mutex","pthread_created: (pointer:function:line) %i:%s \n",x,__FUNCTION__,__LINE__);
+        pthread_mutex_unlock(x);\
+        debug("mutex","unlocking exit: %p (line:fun) %i:%s \n",x,__LINE__, __FUNCTION__);
 #endif
 
 
