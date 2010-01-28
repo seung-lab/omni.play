72d41bfa3085147fbb0f8f4406b06c7ef3056c00
Clay Didier <csdidier@gmail.com>
2010-01-28 22:36:50 +0000

    (original rev 353) Main Page for Doxygen
    git-svn-id: file:///tmp/current/omni/trunk@144 2ffc8166-1744-45da-a82a-d6ea10f7896b

---
+++ omni/src/gui/mainpage.h
@@ -0,0 +1,14 @@
+/** @mainpage
+ *  @author Brett Warme, Rachel Welles Shearer, Matthew Wimer 
+ *  @note . . . others may have contributed . . . 
+ *
+ *  \n\n
+ *  \image html Screenshot.png "Omni is an application for visualizing, editing, and skeletonizing images of the brain." 
+ *
+ *  \n\n\n 
+ *  \n\n\n
+ *  \n\n\n
+ *
+ *  The requirements for this software can be found at: http://gliamac.mit.edu/doxygen/omni/Requirements.pdf \n
+ *  Archived versions can be found at:  http://gliamac.mit.edu/doxygen/omni/archives  
+ */
