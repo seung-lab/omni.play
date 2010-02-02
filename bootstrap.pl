d57e662cb577ff9c7fa845083da781f5fb4a3517
Matthew Wimer <subsilico@yahoo.com>
2010-02-02 19:56:06 +0000

    (original rev 380) take the patch out it breaks the build.
    git-svn-id: file:///tmp/current/omni/trunk@170 2ffc8166-1744-45da-a82a-d6ea10f7896b

--- bootstrap.pl
+++ bootstrap.pl
@@ -37,7 +37,7 @@ sub vtk {
     #`echo "BUILD_SHARED_LIBS:BOOL=OFF" >> $buildPath/$baseFileName/CMakeCache.txt`;
     `echo "CMAKE_BUILD_TYPE:STRING=Debug" >> $buildPath/$baseFileName/CMakeCache.txt`;
 
-    `patch $srcPath/$baseFileName/Utilities/MaterialLibrary/ProcessShader.cxx -i $basePath/external/patches/vtk-processshader.patch`;
+    #`patch $srcPath/$baseFileName/Utilities/MaterialLibrary/ProcessShader.cxx -i $basePath/external/patches/vtk-processshader.patch`;
 
     print "manually run: (cd $buildPath/$baseFileName; ccmake $srcPath/$baseFileName && make && make install)\n";
     print "(make sure to set debug flags!)\n";
