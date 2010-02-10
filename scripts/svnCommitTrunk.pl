bd9e1dfd69ca8bc73af37442b248fcb45b96dc88
Michael Purcaro <purcaro@gmail.com>
2010-02-10 03:31:05 +0000

    (original rev 516) tweaking more!! +++ scripts/svnCommitTrunk.pl	(working copy) --- scripts/svnCommitTrunk.pl	(revision 514)
    git-svn-id: file:///tmp/current/omni/trunk@301 2ffc8166-1744-45da-a82a-d6ea10f7896b

--- scripts/svnCommitTrunk.pl
+++ scripts/svnCommitTrunk.pl
@@ -2,7 +2,7 @@
 
 use strict;
 
-my $src="TRUNKG---------->";
+my $src="TRUNK---------->";
 my $dest="STAGING---------->";
 
 my $pwd = `pwd`;
@@ -22,17 +22,17 @@ if( !-d $staging_folder ) {
 my $diff_add = `svn diff | grep '^+++'`;
 my $diff_sub = `svn diff | grep '^\-\-\-'`;
 
-print "changes:\n";
+print "$src changes:\n";
 print $diff_add;
 print $diff_sub;
 
-print "\n\nplease enter commit message (don't screw up!):  ";
+print "$src please enter commit message (don't screw up!):  ";
 my $commit_msg = <STDIN>;
 
 $commit_msg = $commit_msg.$diff_add.$diff_sub;
 
-print "intended commit msg:\n".$commit_msg;
-print "\nis this ok? control-c to cancel; enter to continue";
+print "$src intended commit msg:\n".$commit_msg;
+print "$src is this ok? (control-c to cancel; enter to continue): ";
 my $ans = <STDIN>;
 
 my $tmpLogFileName = $staging_folder."/tmpLogMsg.txt";
@@ -40,27 +40,27 @@ open OUT_FILE, ">", $tmpLogFileName or die "could not open $tmpLogFileName";
 print OUT_FILE $commit_msg;
 close OUT_FILE;
 
-print "$src: updating src in current folder...";
+print "$src updating src in current folder...";
 `svn up`; 
 print "done\n";
 
-print "$src: committing to repository...";
+print "$src committing to repository...";
 `svn commit -F $tmpLogFileName`;
 print "done\n";
 
-print "$dest: updating...";
+print "$dest updating...";
 `svn up $staging_folder`;
 print "done\n";
 
-print "$dest: merging from trunk...";
+print "$dest merging from trunk...\n";
 print `$staging_folder/external/svnmerge.py merge $staging_folder`;
 print "done\n";
 
-print "$dest: merging into Staging complete; commit the changes? (enter to continue)";
+print "$dest merging into staging complete; commit the changes? (enter to continue)";
 $ans = <STDIN>;
 print "OK\n";
 
-print "dest: committing...";
+print "$dest: committing...";
 print `svn commit -F $tmpLogFileName $staging_folder/`;
 print "done\n";
 
