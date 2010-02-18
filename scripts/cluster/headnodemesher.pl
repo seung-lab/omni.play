1ccf26dee8e3e2bcec06379d6469dffe76ce3390
Michael Purcaro <purcaro@gmail.com>
2010-02-18 19:38:35 +0000

    (original rev 662) output chunk files for cluitering
    git-svn-id: file:///tmp/current/omni/branches/staging@443 2ffc8166-1744-45da-a82a-d6ea10f7896b

--- scripts/cluster/headnodemesher.pl
+++ scripts/cluster/headnodemesher.pl
@@ -1,47 +1,101 @@
 #!/usr/bin/perl -w
 
 use strict;
+use Cwd 'abs_path';
+use File::Basename;
+use POSIX;
 
 my $numMeshProcessesPerHost = 2;
-my $meshinatorHome = $ENV{HOME};
-my $meshinatorHosts = "$meshinatorHome/omnimeshhosts.txt";
 
-open HOSTS, "$meshinatorHosts" or die $!;
+(my $name, my $path, my $suffix) = fileparse( abs_path( $0 ) );
+my $meshinatorHome = $path;
+my $meshinatorHosts = "$meshinatorHome/hosts";
+
+open HOSTS, "$meshinatorHosts" or die "could not find $meshinatorHosts";
 my @hostList = <HOSTS>;
+close HOSTS;
 my $hostCount = scalar @hostList;
+
 open CHUNKS, $ARGV[0] or die $!;
 my @chunks = <CHUNKS>;
+close CHUNKS;
 my $chunkCount = scalar @chunks;
 
-my $maxProcs = $hostCount * $numMeshProcessesPerHost;
-my $chunksPerProccess = $chunkCount / $maxProcs;
-$chunksPerProccess = 1.0 if ($chunksPerProccess lt 1.0);
-
-print "$chunkCount chunks with chunksPerProccess = $chunksPerProccess\n";
-print "$hostCount is hostCount\n";
-
-my $count = 0;
-my $chunkBatch;
 my $cmdCount = 0;
 my @meshCommandChunkInputs;
 my @meshCommandHostInput;
-while ($count < $chunkCount) {
+
+sub byHost {
+    my $chunksPerHost = ceil( $chunkCount / $hostCount );
+    
+    print "$chunkCount chunks with chunksPerHost = $chunksPerHost\n";
+    print "$hostCount is hostCount\n";
+    
+    my $count = 0;
+    my $chunkBatch;
+    $cmdCount = 0;
+    my $nodeCount = 0;
+    while ($count < $chunkCount) {
+	my $chunk = $chunks[$count];
+	$chunkBatch .= $chunk;
+	$count++;
+	if (0 eq $count % $chunksPerHost) {
+	    $meshCommandChunkInputs[$cmdCount] = $chunkBatch;
+	    $meshCommandHostInput[$cmdCount] = $hostList[ $nodeCount ];
+	    $nodeCount++;
+	    chop $meshCommandHostInput[$cmdCount];
+	    $cmdCount++;
+	    $chunkBatch = "";
+	}
+    }
+
+    if ($chunkBatch ne "") {
+	print $chunkBatch;
+	$meshCommandChunkInputs[$cmdCount-1] .= $chunkBatch;
+    }    
+
+}
+
+sub byHostAndProcess 
+{
+    my $maxProcs = $hostCount * $numMeshProcessesPerHost;
+    my $chunksPerProccess = $chunkCount / $maxProcs;
+    $chunksPerProccess = 1.0 if ($chunksPerProccess lt 1.0);
+    
+    print "$chunkCount chunks with chunksPerProccess = $chunksPerProccess\n";
+    print "$hostCount is hostCount\n";
+    
+    my $count = 0;
+    my $chunkBatch;
+    $cmdCount = 0;
+    while ($count < $chunkCount) {
 	my $chunk = $chunks[$count];
 	$chunkBatch .= $chunk;
 	$count++;
 	if (0 eq $count % $chunksPerProccess) {
-		$meshCommandChunkInputs[$cmdCount] = $chunkBatch;
-		$meshCommandHostInput[$cmdCount] = $hostList[$cmdCount % $hostCount];
-		chop $meshCommandHostInput[$cmdCount];
-		$cmdCount++;
-		$chunkBatch = "";
+	    $meshCommandChunkInputs[$cmdCount] = $chunkBatch;
+	    $meshCommandHostInput[$cmdCount] = $hostList[$cmdCount % $hostCount];
+	    chop $meshCommandHostInput[$cmdCount];
+	    $cmdCount++;
+	    $chunkBatch = "";
 	}
+    }
+    if ($chunkBatch ne "") {
+	$meshCommandChunkInputs[$cmdCount-1] .= $chunkBatch;
+    }
 }
-if ($chunkBatch ne "") {
-	$meshCommandChunkInputs[$cmdCount] .= $chunkBatch;
-}
+
+#byHost();
+byHostAndProcess();
+
+`mkdir -p chunk_lists`;
+`rm -f chunk_lists/*`;
 
 print "cmdCount = $cmdCount\n";
 for (my $i = 0; $i < $cmdCount; $i++) {
-	print "Host: " . $meshCommandHostInput[$i]  . " gets...\n" . $meshCommandChunkInputs[$i];
+    my $num = $i;
+    my $outFileName = "chunk_lists/"."chunks--".$meshCommandHostInput[$i].".$num.txt";
+    open OUT_FILE, ">", $outFileName or die "could not write $outFileName";
+    print OUT_FILE $meshCommandChunkInputs[$i];
+    close OUT_FILE;
 }
