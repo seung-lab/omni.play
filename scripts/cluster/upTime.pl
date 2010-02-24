9cc145b08952df60193ae03cdfbcf6cd552e2abe
Matthew Wimer <subsilico@yahoo.com>
2010-02-24 10:40:15 +0000

    (original rev 756) iretunrs wrong value now
    git-svn-id: file:///tmp/current/omni/branches/staging@537 2ffc8166-1744-45da-a82a-d6ea10f7896b

---
+++ scripts/cluster/upTime.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use Thread;
+
+my $flag = "";
+if (-t STDIN) {
+}
+
+
+sub runNode 
+{
+    my $node = $_[0];
+    my $uptime = `ssh ${node} uptime | cut -f15 -d\\ `;
+    print $uptime;
+
+   print $node;
+   exit(0);
+}
+
+open IN_FILE,  "<", "hosts"  or die "could not read hosts";
+
+my @threads;
+while (my $line = <IN_FILE>) {
+    chomp( $line );
+    my $thr = new Thread \&runNode, $line;
+    push(@threads, $thr);
+}
+close IN_FILE;
+
+my $count=0;
+foreach my $thread (@threads){ 
+    $thread->join;
+    $count = $count +1;
+}
