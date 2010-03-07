f9703fd10e2d163933944784a35fe0b19ae48769
Michael Purcaro <purcaro@gmail.com>
2010-03-07 22:04:03 +0000

    (original rev 919) reduce whitespace
    git-svn-id: file:///tmp/current/omni/branches/staging@700 2ffc8166-1744-45da-a82a-d6ea10f7896b

--- omni/src/mainpage.h
+++ omni/src/mainpage.h
@@ -5,9 +5,7 @@
  *  \n\n
  *  \image html Screenshot.png "Omni is an application for visualizing, editing, and skeletonizing images of the brain." 
  *
- *  \n\n\n 
- *  \n\n\n
- *  \n\n\n
+ *  \n\n
  *
  *  The requirements for this software can be found at: http://gliamac.mit.edu/doxygen/omni/Requirements.pdf \n
  *  Archived versions can be found at: \htmlonly <A HREF="http://gliamac.mit.edu/doxygen/omni/archives" TARGET="_blank">
