57f71d56dd64804298e52fc42e9d401ff31af7da
Clay Didier <csdidier@gmail.com>
2010-03-15 19:02:29 +0000

    (original rev 983) Recommend everyone check out hdf5-tools a set of command line tools for hdf5 I noticed it didn't have anyway of copying files from within an hdf5 file to your local drive so I've created one called h5get usage:   h5get [omni-file] [*.dat file-name]
    git-svn-id: file:///tmp/current/omni/branches/staging@764 2ffc8166-1744-45da-a82a-d6ea10f7896b

---
+++ scripts/hdf5/h5get.c
@@ -0,0 +1,31 @@
+#include "hdf5.h"
+#include "czip.h"
+
+int main(int argc, char* argv[])
+{
+	hid_t dataset,filespace,file;
+	void* data;
+	file = H5Fopen (argv[1], H5F_ACC_RDWR, H5P_DEFAULT);
+	dataset = H5Dopen(file,argv[2]);
+	filespace = H5Dget_space (dataset);
+	int number = H5Sget_simple_extent_npoints(filespace);
+	int size = H5Sget_simple_extent_type(filespace);
+	if (size == sizeof(uint32_t)){
+		data = (uint32_t*) malloc(number*size);
+		H5Dread(dataset, H5T_NATIVE_UINT32, H5S_ALL, H5S_ALL, H5P_DEFAULT,data);
+	} else if (size == sizeof(char)){
+		data = (char*) malloc(number*size);
+		H5Dread(dataset, H5T_NATIVE_CHAR, H5S_ALL, H5S_ALL, H5P_DEFAULT,data);		
+	} else {
+		printf("Data Element Size = %i \n",size);
+	}
+
+	FILE* afile;
+	afile =fopen("./output.dat","wb");
+	if (afile != NULL){
+		fwrite(data,number,size,afile);
+	} else {
+		printf("Could not open file %s \n",argv[2]);
+	}
+	fclose(afile);
+}
