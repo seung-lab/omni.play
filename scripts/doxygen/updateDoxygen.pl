18423a81a46f0d1176adea8254fa6ba23aa50a8e
Michael Purcaro <purcaro@gmail.com>
2010-03-18 07:29:34 +0000

    (original rev 1010) starting to flesh out update script
    git-svn-id: file:///tmp/current/omni/branches/staging@791 2ffc8166-1744-45da-a82a-d6ea10f7896b

--- scripts/doxygen/updateDoxygen.pl
+++ scripts/doxygen/updateDoxygen.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/perl -w
 
 use strict;
+use XML::Simple;
 
 my $root = "/Users/purcaro/gliamacDoxygen";
+my $main_output = $root."/www/doxygenOutput";
 my @folders = ("omni.src.dev",
 	       "omni.src.stable",
 	       "omni.src.staging",
@@ -10,12 +12,22 @@ my @folders = ("omni.src.dev",
 
 foreach (@folders) {
     my $folder = $_;
-    my $path = $root."/".$folder;
-    
-    my $svn_ver = `svnversion $path`;
-    $svn_ver =~ s/(.*)\://;
-    $svn_ver =~ s/M//;
 
+    my $path = $root."/".$folder;   
+
+    `svn up $path`;
+
+    my $xml_log = `svn log -l 1 --with-no-revprops --xml $path`;
+    my $xs1 = XML::Simple->new();
+    my $doc = $xs1->XMLin($xml_log);
+    my $svn_ver = $doc->{logentry}->{revision};
+
+    print $path." (".$svn_ver.")\n";
+
+    my $output_folder = $main_output."/".$folder."/".$svn_ver;
+    `mkdir -p $output_folder`;
+
+    
 }
 
 
