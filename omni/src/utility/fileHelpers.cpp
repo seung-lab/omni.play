da22ec406f50f45d21e54dab75740ae6e3e54486
Michael Purcaro <purcaro@gmail.com>
2010-03-19 20:29:08 +0000

    (original rev 1048) use correct function!
    git-svn-id: file:///tmp/current/omni/branches/staging@823 2ffc8166-1744-45da-a82a-d6ea10f7896b

--- omni/src/utility/fileHelpers.cpp
+++ omni/src/utility/fileHelpers.cpp
@@ -1,13 +1,15 @@
 #include "fileHelpers.h"
 #include <QFileInfo>
+#include "common/omDebug.h"
 
 bool FileHelpers::isFileReadOnly( QString fileNameAndPath )
 {
 	bool isReadOnly = false;
 	QFileInfo file(fileNameAndPath);
-	if (file.exists() && !file.permission(QFile::WriteUser)){
+	if (file.exists() && !file.isWritable() ){
 		isReadOnly = true; 
 	}
 	
+	debug("fileHelpers", "%s: file is read only? %d\n", __FUNCTION__, isReadOnly );
 	return isReadOnly;
  }
