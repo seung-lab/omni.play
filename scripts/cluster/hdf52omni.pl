fd61aedffcadfe15bc7028b54893de8c119ad6dd
Michael Purcaro <purcaro@gmail.com>
2010-03-20 04:27:45 +0000

    (original rev 1049) tweaks from Matt
    git-svn-id: file:///tmp/current/omni/branches/staging@824 2ffc8166-1744-45da-a82a-d6ea10f7896b

--- scripts/cluster/hdf52omni.pl
+++ scripts/cluster/hdf52omni.pl
@@ -12,7 +12,11 @@ my $meshinatorHome = $path;
 
 # Create File
 my $project = abs_path($ARGV[0]);
+chomp($project);
 my $createFileLine = "create:".$project."\n";
+
+print "will create \"$createFileLine\"\n";
+
 my $full =  "$project.full";
 print `unlink $full`;
 my $plan = "$project.plan";
@@ -23,6 +27,7 @@ sub runLinesRaw
 	$project = $_[1] if (defined $_[1]);
 	open CMD, ">.my.omni.cmd" or die $!;
 	print CMD $_[0];
+	print "running; $meshinatorHome/../../omni/bin/omni --headless=.my.omni.cmd $project";
 	print `$meshinatorHome/../../omni/bin/omni --headless=.my.omni.cmd $project`;
 }
 
@@ -31,21 +36,26 @@ sub runLinesProject
 	runLinesRaw($_[0], $_[1]);
 }
 
-runLinesRaw($createFileLine);
+#runLinesRaw($createFileLine);
 
 # Add Segmentations
-my @h5s = $ARGV[1 .. scalar @ARGV-1];
+my @h5s = @ARGV[1 .. scalar @ARGV-1];
+#print @h5s;
+#exit(0);
 my $seg = 0;
 foreach my $h5 (@h5s)
 {
 	$seg++;
-	my $addSegLines = "addSegmentationFromHDF5:".abs_path($h5)."\n";
+	my $addSegLines = $createFileLine;
+	$addSegLines .= "addSegmentationFromHDF5:".abs_path($h5)."\n";
  	$addSegLines .= "seg:".$seg."\n";
  	$addSegLines .= "meshplan\n";
 
 	runLinesProject($addSegLines);
 	print `cat $plan >> $full`; 
 }
+
+exit(0);
 
 print `cat $full > $plan`; 
 print `$meshinatorHome/headnodemesher.pl $plan`;
