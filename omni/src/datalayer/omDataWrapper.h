2b9bc8411a54eae3c3bd1aef10d2c924f8d167ff
Michael Purcaro <purcaro@gmail.com>
2010-05-31 22:33:12 +0000

    (original rev 1612) add a valid flag for omdatawrapper; not really used correctly (yet)
    git-svn-id: file:///tmp/current/omni/branches/staging@1379 2ffc8166-1744-45da-a82a-d6ea10f7896b

--- omni/src/datalayer/omDataWrapper.h
+++ omni/src/datalayer/omDataWrapper.h
@@ -6,8 +6,15 @@
 class OmDataWrapper : boost::noncopyable
 {
  public:
+	OmDataWrapper()
+		: mData(NULL)
+		, isValid(false)
+	{
+	}
+
 	OmDataWrapper( void * ptr )
 		: mData(ptr)
+		, isValid( true )
 	{
 	}
 
@@ -15,17 +22,18 @@ class OmDataWrapper : boost::noncopyable
 		free(mData); // from malloc in hdf5...
 	}
 
-	char * getCharPtr(){ return (char*)mData; }
-	quint8 * getQuint8Ptr(){ return (quint8*)mData; }
-	uint32_t * getUInt32Ptr(){ return (uint32_t *)mData; }
-	GLuint * getGLuintPtr(){ return (GLuint *)mData; }
-	GLfloat * getGLfloatPtr(){ return (GLfloat *)mData; }
-	quint32 * getQuint32Ptr(){ return (quint32 *)mData; }
-	float * getFloatPtr(){ return (float*)mData; }
-	void * getPtr(){ return mData; }
+	char * getCharPtr(){ assert(isValid); return (char*)mData; }
+	quint8 * getQuint8Ptr(){ assert(isValid); return (quint8*)mData; }
+	uint32_t * getUInt32Ptr(){ assert(isValid); return (uint32_t *)mData; }
+	GLuint * getGLuintPtr(){ assert(isValid); return (GLuint *)mData; }
+	GLfloat * getGLfloatPtr(){ assert(isValid); return (GLfloat *)mData; }
+	quint32 * getQuint32Ptr(){ assert(isValid); return (quint32 *)mData; }
+	float * getFloatPtr(){ assert(isValid); return (float*)mData; }
+	void * getPtr(){ assert(isValid); return mData; }
 
  private:	
 	void * mData;
+	const bool isValid;
 };
 
 typedef boost::shared_ptr<OmDataWrapper> OmDataWrapperPtr;
