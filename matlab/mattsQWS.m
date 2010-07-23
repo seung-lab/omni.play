be4ebbd5b29f61b8cf1454dfc259e83e8096e219
Matthew Wimer <subsilico@yahoo.com>
2010-07-23 03:43:25 +0000

    (original rev 2222) new plan
    git-svn-id: file:///tmp/current/omni/branches/staging@1982 2ffc8166-1744-45da-a82a-d6ea10f7896b

--- matlab/mattsQWS.m
+++ matlab/mattsQWS.m
@@ -1,8 +1,41 @@
+
 function mattsQWS
-	conn  = hdf5read('/home/sturaga/exhibit/608/e2006_im_histeq_epoch1769.h5', '/main');
-	LoThreshold=0.1, HiThreshold=0.98, SizeThreshold=200;
-	[seg graph graphValues dend dendValues] = QuickieWS(conn, LoThreshold, HiThreshold, SizeThreshold);
-	clear conn;
+	fid = fopen('mattsBuild.cmd', 'w');
+
+	conn = hdf5read('/home/sturaga/exhibit/608/e2006_im_histeq_epoch1769.h5', '/main');
+	conn = conn(15:550+15,15:550+15,15:550+15,:);
+
 	chan = hdf5read('/home/sturaga/net_sets/E2006/point_cloud/im.h5', '/main');
-	hdf5write('/home/mwimer/mattsQWS.h5', '/main', seg, '/dend', dend, '/graph', graph, '/dendValues', dendValues, '/chanSingle', chan);
+	chan = chan(15:550+15,15:550+15,15:550+15,:);
+
+	for inc = 100:100:400
+		% Careful, these need to be on the outside most loop.
+		chan = chan(1:550-inc,1:550-inc,1:550-inc,:);
+		conn = conn(1:550-inc,1:550-inc,1:550-inc,:);
+
+		for absmin = [0.9 .6 .3]
+			for hithres = [.995 .980 1.0 .990] 
+				for threshold = [3000 300 30]
+					LoThreshold=0.1, HiThreshold=hithres, SizeThreshold=threshold;
+					[seg graph graphValues dend dendValues] = QuickieWS(conn, LoThreshold, HiThreshold, SizeThreshold, absmin);
+
+					f = sprintf ('meshinator/new-%d-%d-%f-%f', 550-inc, threshold, hithres, absmin);
+					fname = sprintf ('/home/mwimer/%s.h5', f);
+					fprintf (fid, 'create:/home/mwimer/%s.omni\n', f);
+					fprintf (fid, 'loadHDF5seg:/home/mwimer/%s.h5\n', f);
+					fprintf (fid, 'mesh\n');
+					fprintf (fid, 'loadHDF5chan:/home/mwimer/%s.h5\n', f);
+					fprintf (fid, 'close\n');
+
+					hdf5write(fname, '/main', seg, '/dend', dend, '/graph', graph, '/dendValues', dendValues, '/chanSingle', chan);
+				end
+			end
+		end
+	end
 end
+
+%format
+%create:/Users/purcaro/Omni/purcaro_testing/regionGraphs/i1088_150_graph.omni
+%loadHDF5seg:/Users/purcaro/Omni/data/regionGraphs/i1088_150_graph.h5
+%mesh
+
