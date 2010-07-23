6ddcb9c2735ef36a2142b75897a6d2cea6f82a0e
Matthew Wimer <subsilico@yahoo.com>
2010-07-23 16:43:10 +0000

    (original rev 2239) testing
    git-svn-id: file:///tmp/current/omni/branches/staging@1999 2ffc8166-1744-45da-a82a-d6ea10f7896b

--- matlab/mattsQWS.m
+++ matlab/mattsQWS.m
@@ -2,24 +2,24 @@
 function mattsQWS
 	fid = fopen('mattsBuild.cmd', 'w');
 
-	conn = hdf5read('/home/sturaga/exhibit/608/e2006_im_histeq_epoch1769.h5', '/main');
+	conn = hdf5read('/home/mwimer/e2006_im_histeq_epoch1769.h5', '/main');
 	conn = conn(15:550+15,15:550+15,15:550+15,:);
 
-	chan = hdf5read('/home/sturaga/net_sets/E2006/point_cloud/im.h5', '/main');
+	chan = hdf5read('/home/mwimer/im.h5', '/main');
 	chan = chan(15:550+15,15:550+15,15:550+15,:);
 
-	for inc = 100:100:400
+	for inc = 400:100:400
 		% Careful, these need to be on the outside most loop.
 		chan = chan(1:550-inc,1:550-inc,1:550-inc,:);
 		conn = conn(1:550-inc,1:550-inc,1:550-inc,:);
 
 		for absmin = [0.9 .6 .3]
-			for hithres = [.995 .980 1.0 .990] 
-				for threshold = [3000 300 30]
+			for hithres = [.995 .982 1.0] 
+				for threshold = [30 3000 300 60 15]
 					LoThreshold=0.1, HiThreshold=hithres, SizeThreshold=threshold;
 					[seg graph graphValues dend dendValues] = QuickieWS(conn, LoThreshold, HiThreshold, SizeThreshold, absmin);
 
-					f = sprintf ('meshinator/new-%d-%d-%f-%f', 550-inc, threshold, hithres, absmin);
+					f = sprintf ('test-%d-%d-%f-%f', 550-inc, threshold, hithres, absmin);
 					fname = sprintf ('/home/mwimer/%s.h5', f);
 					fprintf (fid, 'create:/home/mwimer/%s.omni\n', f);
 					fprintf (fid, 'loadHDF5seg:/home/mwimer/%s.h5\n', f);
@@ -27,7 +27,7 @@ function mattsQWS
 					fprintf (fid, 'loadHDF5chan:/home/mwimer/%s.h5\n', f);
 					fprintf (fid, 'close\n');
 
-					hdf5write(fname, '/main', seg, '/dend', dend, '/graph', graph, '/dendValues', dendValues, '/chanSingle', chan);
+					hdf5write(fname, '/main', seg, '/dend', dend, '/graph', graph, '/dendValues', dendValues, '/chanSingle', chan, '/affGraphSingle', conn);
 				end
 			end
 		end
