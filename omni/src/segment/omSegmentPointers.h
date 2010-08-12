5557943c9a11c2c41accb88fbd5e6fea074e0397
Michael Purcaro <purcaro@gmail.com>
2010-08-12 07:03:08 +0000

    (original rev 2413) more cleanup of 3d mesh segment list
    git-svn-id: file:///tmp/current/omni/trunk@2170 2ffc8166-1744-45da-a82a-d6ea10f7896b

--- omni/src/segment/omSegmentPointers.h
+++ omni/src/segment/omSegmentPointers.h
@@ -5,37 +5,12 @@
 
 typedef std::vector<OmSegment*> OmSegPtrList;
 
-class OmSegPtrListValid {
- public:
- 	OmSegPtrListValid()
-		: isValid(false) 
-		, freshness(0)
-		, isFetching(false)
-	{}
- 	explicit OmSegPtrListValid(const bool isFetching)
-		: isValid(false) 
-		, freshness(0)
-		, isFetching(isFetching)
-	{}
- 	OmSegPtrListValid( const OmSegPtrList & L, const quint32 f )
-		: isValid(true)
-		, list(L) 
-		, freshness(f)
-		, isFetching(false)
-	{}
-	
-	bool isValid;
-	OmSegPtrList list;
-	quint32 freshness;
-	bool isFetching;
-};
-
 class OmSegPtrListWithPage {
  public:
 	OmSegPtrListWithPage( const OmSegPtrList & L, int pageOffset )
 		: mPageOffset(pageOffset)
 		, list(L) {}
-	
+
 	int mPageOffset;
 	OmSegPtrList list;
 };
@@ -45,7 +20,7 @@ class OmSegIDsListWithPage {
 	OmSegIDsListWithPage( const OmSegIDsList & L, int pageOffset )
 		: mPageOffset(pageOffset)
 		, list(L) {}
-	
+
 	int mPageOffset;
 	OmSegIDsList list;
 };
