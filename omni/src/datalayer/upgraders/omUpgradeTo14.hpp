173cb9a9aeecb94741636ef9f9e430edc796bb3a
Michael Purcaro <purcaro@gmail.com>
2010-09-20 21:47:14 +0000

    (original rev 2854) forgot these
    git-svn-id: file:///tmp/current/omni/branches/staging@2607 2ffc8166-1744-45da-a82a-d6ea10f7896b

---
+++ omni/src/datalayer/upgraders/omUpgradeTo14.hpp
@@ -0,0 +1,11 @@
+#ifndef OM_UPGRADE_TO_14_HPP
+#define OM_UPGRADE_TO_14_HPP
+
+// extract volumes from hdf5 to mem-map
+
+class OmUpgradeTo14{
+public:
+
+};
+
+#endif
---
+++ omni/src/datalayer/upgraders/omUpgraders.hpp
@@ -0,0 +1,14 @@
+#ifndef OM_UPGRADERS_HPP
+#define OM_UPGRADERS_HPP
+
+
+
+class OmUpgraders{
+public:
+	static void to14(){
+
+	}
+
+};
+
+#endif
