9405d1243e37d4159a0841b067164057cecd4f2f
Michael Purcaro <purcaro@gmail.com>
2010-09-22 03:22:05 +0000

    (original rev 2888) more merging dev into staging
    git-svn-id: file:///tmp/current/omni/branches/staging@2641 2ffc8166-1744-45da-a82a-d6ea10f7896b

---
+++ omni/src/datalayer/omIDataReader.h
@@ -0,0 +1,28 @@
+#ifndef OM_DATA_READER_H
+#define OM_DATA_READER_H
+
+#include "common/omCommon.h"
+#include "datalayer/omDataPath.h"
+#include "datalayer/omDataWrapper.h"
+
+class OmIDataReader {
+public:
+	virtual ~OmIDataReader(){}
+
+	virtual QString getFileNameAndPath() = 0;
+
+	//file
+	virtual void open() = 0;
+	virtual void close() = 0;
+
+	virtual bool group_exists( const OmDataPath & path ) = 0;
+
+	virtual bool dataset_exists( const OmDataPath & path ) = 0;
+
+	virtual OmDataWrapperPtr readDataset( const OmDataPath & path, int* size = NULL) = 0;
+	virtual Vector3i getDatasetDims(const OmDataPath & path) = 0;
+
+	virtual Vector3i getChunkedDatasetDims(const OmDataPath & path ) = 0;
+	virtual OmDataWrapperPtr readChunk( const OmDataPath & path, DataBbox dataExtent)=0;
+};
+#endif
---
+++ omni/src/datalayer/omIDataWriter.h
@@ -0,0 +1,39 @@
+#ifndef OM_DATA_WRITER_H
+#define OM_DATA_WRITER_H
+
+#include "datalayer/omDataWrapper.h"
+#include "common/omCommon.h"
+#include "volume/omVolumeTypes.hpp"
+
+class OmDataPath;
+
+class OmIDataWriter {
+public:
+	virtual ~OmIDataWriter(){}
+
+	virtual void create() = 0;
+
+	virtual void open() = 0;
+	virtual void close() = 0;
+	virtual void flush() = 0;
+
+	//group
+	virtual void group_delete( const OmDataPath & path ) = 0;
+
+	//image I/O
+	virtual void allocateChunkedDataset( const OmDataPath &,
+					     const Vector3i&,
+					     const Vector3i&,
+					     const OmVolDataType) = 0;
+
+	virtual void writeChunk(const OmDataPath &,
+				DataBbox,
+				OmDataWrapperPtr) = 0;
+
+	//data set raw
+	virtual void writeDataset(const OmDataPath&,
+				  int,
+				  const OmDataWrapperPtr) = 0;
+};
+
+#endif
