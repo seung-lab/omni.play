e246d655d422b04b64caa058ed8f33d1a034a61f
Michael Purcaro <purcaro@gmail.com>
2010-09-26 07:47:20 +0000

    (original rev 2978) forgot these
    git-svn-id: file:///tmp/current/omni/branches/staging@2731 2ffc8166-1744-45da-a82a-d6ea10f7896b

---
+++ omni/src/gui/inspectors/segmentation/exportButton.hpp
@@ -0,0 +1,34 @@
+#ifndef SEG_INSPECTOR_EXPORT_BUTTON_H
+#define SEG_INSPECTOR_EXPORT_BUTTON_H
+
+#include "gui/widgets/omButton.h"
+#include "gui/inspectors/segmentation/segInspector.h"
+#include "utility/dataWrappers.h"
+
+class ExportButton : public OmButton<SegInspector> {
+public:
+	ExportButton(SegInspector * d)
+		: OmButton<SegInspector>( d,
+								  "Export",
+								  "Export",
+								  false)
+	{
+	}
+
+private:
+	void doAction()
+	{
+		boost::shared_ptr<SegmentationDataWrapper> sdw =
+			mParent->getSegmentationDataWrapper();
+
+		const QString fileName =
+			QFileDialog::getSaveFileName(this, tr("Export As"));
+
+		if (fileName == NULL)
+			return;
+
+		sdw->getSegmentation().ExportInternalData(fileName);
+	}
+};
+
+#endif
---
+++ omni/src/gui/inspectors/segmentation/meshPreviewButton.hpp
@@ -0,0 +1,28 @@
+#ifndef SEG_INSPECTOR_MESH_PREVIEW_BUTTON_H
+#define SEG_INSPECTOR_MESH_PREVIEW_BUTTON_H
+
+#include "gui/widgets/omButton.h"
+#include "gui/inspectors/segmentation/segInspector.h"
+#include "gui/meshPreviewer/meshPreviewer.hpp"
+
+class MeshPreviewButton : public OmButton<SegInspector> {
+public:
+	MeshPreviewButton(SegInspector * d)
+		: OmButton<SegInspector>( d,
+								  "Export",
+								  "Export",
+								  false)
+	{
+	}
+
+private:
+	void doAction()
+	{
+		boost::shared_ptr<SegmentationDataWrapper> sdw =
+			mParent->getSegmentationDataWrapper();
+
+		new MeshPreviewer(this, sdw, mParent->getViewGroupState());
+	}
+};
+
+#endif
