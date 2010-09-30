8063f88ed77e12c03738f61677429783825fae93
Michael Purcaro <purcaro@gmail.com>
2010-09-30 20:49:55 +0000

    (original rev 3030) mutlithread downsampler
    git-svn-id: file:///tmp/current/omni/branches/staging@2783 2ffc8166-1744-45da-a82a-d6ea10f7896b

--- omni/src/volume/build/omDownsampler.hpp
+++ omni/src/volume/build/omDownsampler.hpp
@@ -2,89 +2,68 @@
 #define OM_DOWNSAMPLER_HPP
 
 #include "volume/omMipVolume.h"
+#include "zi/omThreads.h"
 
 template <typename T>
-class OmDownsampler {
+struct MipLevelInfo {
+	T* data;
+	uint64_t factor;
+	Vector3<uint64_t> volDims;
+
+	uint64_t maxAllowedIndex;
+	uint64_t volSlabSize;
+	uint64_t volSliceSize;
+	uint64_t volRowSize;
+	uint64_t totalVoxels;
+};
+
+struct MippingInfo {
+	int maxMipLevel;
+	Vector3<uint64_t> chunkDims;
+	uint64_t chunkSize;
+	uint64_t tileSize;
+};
+
+template <typename T>
+class DownsampleVoxelTask : public zi::runnable {
 private:
-	struct MipLevelInfo {
-		T* data;
-		uint64_t factor;
-		Vector3<uint64_t> volDims;
-
-		uint64_t maxAllowedIndex;
-		uint64_t volSlabSize;
-		uint64_t volSliceSize;
-		uint64_t volRowSize;
-		uint64_t totalVoxels;
-	};
-
-	OmMipVolume *const vol_;
-	std::vector<MipLevelInfo> mips_;
-	const int maxMipLevel_;
-
-	const Vector3<uint64_t> chunkDims_;
-	const uint64_t chunkSize_;
-	const uint64_t tileSize_;
+	const std::vector<MipLevelInfo<T> >& mips_;
+	const MippingInfo& mippingInfo_;
+	const uint64_t sy_;
+	const uint64_t sz_;
 
 public:
-	OmDownsampler(OmMipVolume* vol)
-		: vol_(vol)
-		, maxMipLevel_(vol_->GetRootMipLevel())
-		, chunkDims_(vol_->GetChunkDimensions())
-		, chunkSize_(chunkDims_.x * chunkDims_.y * chunkDims_.z)
-		, tileSize_(chunkDims_.x * chunkDims_.y)
+	DownsampleVoxelTask(const std::vector<MipLevelInfo<T> >& mips,
+						const MippingInfo& mippingInfo,
+						const uint64_t sy, const uint64_t sz)
+		: mips_(mips)
+		, mippingInfo_(mippingInfo)
+		, sy_(sy), sz_(sz)
+	{}
+
+	void run()
 	{
-		mips_.resize(maxMipLevel_ + 1);
-
-		for(int i=0; i <= maxMipLevel_; ++i){
-			mips_[i].data = boost::get<T*>(vol_->getVolData()->GetVolPtr(i));
-			mips_[i].factor = OMPOW(2, i);
-
-			const Vector3i dims = vol_->getDimsRoundedToNearestChunk(i);
-			mips_[i].volDims = dims;
-			mips_[i].volSlabSize = dims.x * dims.y * chunkDims_.z;
-			mips_[i].volSliceSize = dims.x * dims.y;
-			mips_[i].volRowSize = dims.x * chunkDims_.y * chunkDims_.z;
-			mips_[i].totalVoxels = dims.x * dims.y * dims.z;
+		for(uint64_t sx=0; sx < mips_[0].volDims.x; sx+=2){
+			pushVoxelIntoMips(DataCoord(sx, sy_, sz_));
 		}
 	}
 
-	void DownsampleChooseOneVoxelOfNine()
-	{
-		printf("downsampling...\n");
-		for(uint64_t sz=0; sz < mips_[0].volDims.z; sz+=2){
-			for(uint64_t sy=0; sy < mips_[0].volDims.y; sy+=2){
-				for(uint64_t sx=0; sx < mips_[0].volDims.x; sx+=2){
-					pushVoxelIntoMips(DataCoord(sx, sy, sz));
-				}
-			}
-		}
-		printf("\t downsampling done\n");
-	}
-
-	uint64_t getOffsetIntoVolume(const DataCoord& coord, const int level,
-								 const bool debug = false)
+	uint64_t getOffsetIntoVolume(const DataCoord& coord, const int level)
 	{
 		const DataCoord chunkPos =
-			OmMipVolume::DataToMipCoord(coord, level, chunkDims_).Coordinate;
-
-		if(debug){
-			std::cout << "chunkPos is " << chunkPos << "\n";
-		}
+			OmMipVolume::DataToMipCoord(coord, level,
+										mippingInfo_.chunkDims).Coordinate;
 
 		const uint64_t chunkOffsetFromVolStart =
 			mips_[level].volSlabSize * chunkPos.z +
 			mips_[level].volRowSize  * chunkPos.y +
-			chunkSize_ * chunkPos.x;
+			mippingInfo_.chunkSize * chunkPos.x;
 
-		const Vector3i offsetVec = coord - chunkPos*chunkDims_;
-		if(debug){
-			std::cout << "offsetVec is " << offsetVec << "\n";
-		}
+		const Vector3i offsetVec = coord - chunkPos*mippingInfo_.chunkDims;
 
 		const uint64_t offsetIntoChunk =
-			tileSize_ * offsetVec.z +
-			chunkDims_.x * offsetVec.y +
+			mippingInfo_.tileSize    * offsetVec.z +
+			mippingInfo_.chunkDims.x * offsetVec.y +
 			offsetVec.x;
 
 		return chunkOffsetFromVolStart + offsetIntoChunk;
@@ -92,7 +71,7 @@ public:
 
 	void pushVoxelIntoMips(const DataCoord& srcCoord)
 	{
-		for(int i = 1; i <= maxMipLevel_; ++i){
+		for(int i = 1; i <= mippingInfo_.maxMipLevel; ++i){
 			if( 0 != srcCoord.z % mips_[i].factor ||
 				0 != srcCoord.y % mips_[i].factor ||
 				0 != srcCoord.x % mips_[i].factor ){
@@ -104,24 +83,65 @@ public:
 			const uint64_t dstOffset = getOffsetIntoVolume(dstCoord, i);
 			const uint64_t srcOffset = getOffsetIntoVolume(srcCoord, 0);
 
-			if( dstOffset >= mips_[i].totalVoxels ||
-				srcOffset >= mips_[0].totalVoxels ){
-
-				std::cout << "src coord: " << srcCoord
-						  << ", dst coord: " << dstCoord << "\n";
-				std::cout << "\tsrc offset: " << srcOffset
-						  << ", dst offset: " << dstOffset << "\n";
-				std::cout << "\tsrc max offset: " << mips_[0].totalVoxels << "\n"
-						  << ", dst max offset: " << mips_[i].totalVoxels << "\n";
-				std::cout << "src\n********\n" << getOffsetIntoVolume(srcCoord, 0, true);
-				std::cout << "\ndst\n********\n" << getOffsetIntoVolume(dstCoord, i, true);
-				fflush(stdout);
-				assert(0);
-			}
-
 			mips_[i].data[dstOffset] = mips_[0].data[srcOffset];
 		}
+	}
+};
 
+
+template <typename T>
+class OmDownsampler {
+private:
+	OmMipVolume *const vol_;
+	std::vector<MipLevelInfo<T> > mips_;
+	MippingInfo mippingInfo_;
+
+public:
+	OmDownsampler(OmMipVolume* vol)
+		: vol_(vol)
+	{
+		mippingInfo_.maxMipLevel = vol_->GetRootMipLevel();
+
+		const Vector3<uint64_t> chunkDims = vol_->GetChunkDimensions();
+
+		mippingInfo_.chunkDims = chunkDims;
+		mippingInfo_.chunkSize = chunkDims.x * chunkDims.y * chunkDims.z;
+		mippingInfo_.tileSize = chunkDims.x * chunkDims.y;
+
+		mips_.resize(mippingInfo_.maxMipLevel + 1);
+
+		for(int i=0; i <= mippingInfo_.maxMipLevel; ++i){
+			mips_[i].data = boost::get<T*>(vol_->getVolData()->GetVolPtr(i));
+			mips_[i].factor = OMPOW(2, i);
+
+			const Vector3i dims = vol_->getDimsRoundedToNearestChunk(i);
+			mips_[i].volDims = dims;
+			mips_[i].volSlabSize = dims.x * dims.y * chunkDims.z;
+			mips_[i].volSliceSize = dims.x * dims.y;
+			mips_[i].volRowSize = dims.x * chunkDims.y * chunkDims.z;
+			mips_[i].totalVoxels = dims.x * dims.y * dims.z;
+		}
+	}
+
+	void DownsampleChooseOneVoxelOfNine()
+	{
+		printf("downsampling...\n");
+
+		OmThreadPool threadPool;
+		threadPool.start(30);
+
+		for(uint64_t sz=0; sz < mips_[0].volDims.z; sz+=2){
+			for(uint64_t sy=0; sy < mips_[0].volDims.y; sy+=2){
+					boost::shared_ptr<DownsampleVoxelTask<T> > task =
+						boost::make_shared<DownsampleVoxelTask<T> >(mips_,
+																	mippingInfo_,
+																	sy, sz);
+					threadPool.addTaskBack(task);
+			}
+		}
+
+		threadPool.join();
+		printf("\t downsampling done\n");
 	}
 };
 
