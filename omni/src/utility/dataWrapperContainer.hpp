a642fd7de3dec06816271e383e87818a026bf9f8
Michael Purcaro <purcaro@gmail.com>
2010-10-07 00:58:41 +0000

    (original rev 3109) cleanup datawrappercontainer
    git-svn-id: file:///tmp/current/omni/branches/staging@2862 2ffc8166-1744-45da-a82a-d6ea10f7896b

--- omni/src/utility/dataWrapperContainer.hpp
+++ omni/src/utility/dataWrapperContainer.hpp
@@ -5,44 +5,60 @@
 
 class DataWrapperContainer {
 private:
-	boost::optional<SegmentationDataWrapper> segmenDW;
-	boost::optional<ChannelDataWrapper> cDW;
-	ObjectType mType;
+	boost::optional<SegmentationDataWrapper> sdw_;
+	boost::optional<ChannelDataWrapper> cdw_;
 
 public:
 	DataWrapperContainer(){}
 
 	DataWrapperContainer(const ObjectType obj_type, const OmId obj_id)
-		: mType(obj_type)
 	{
-		switch (obj_type) {
+		switch (obj_type){
 		case CHANNEL:
-			cDW = boost::optional<ChannelDataWrapper>(obj_id);
+			cdw_ = boost::optional<ChannelDataWrapper>(obj_id);
 			break;
 		case SEGMENTATION:
-			segmenDW = boost::optional<SegmentationDataWrapper>(obj_id);
+			sdw_ = boost::optional<SegmentationDataWrapper>(obj_id);
 			break;
+		default:
+			throw OmArgException("unknown type");
 		}
 	}
 
 	explicit DataWrapperContainer(SegmentationDataWrapper sdw)
 	{
-		mType = SEGMENTATION;
-		segmenDW = boost::optional<SegmentationDataWrapper>(sdw);
+		sdw_ = boost::optional<SegmentationDataWrapper>(sdw);
 	}
 
 	bool isValidContainer() const {
-		return segmenDW || cDW;
+		return sdw_ || cdw_;
 	}
 
-	ObjectType getType() const {
-		return mType;
+	bool isSegmentation() const {
+		return sdw_;
 	}
+
+	bool isChannel() const {
+		return cdw_;
+	}
+
+	ObjectType getType() const
+	{
+		if(cdw_){
+			return CHANNEL;
+		}
+		if(sdw_){
+			return SEGMENTATION;
+		}
+		throw OmArgException("uninitialized");
+	}
+
 	SegmentationDataWrapper getSegmentationDataWrapper() const {
-		return *segmenDW;
+		return *sdw_;
 	}
+
 	ChannelDataWrapper getChannelDataWrapper() const {
-		return *cDW;
+		return *cdw_;
 	}
 };
 
