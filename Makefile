0c1f592b3a8d52333440d51c9d00140ebd1bbf42
Sebastien <sdabdoub@mit.edu>
2011-11-04 12:30:33 -0400

    initial commit

---
+++ Makefile
@@ -0,0 +1,17 @@
+#all:
+#	g++ data.cpp  -I./zi_lib -I./zi_lib/external/include -lpthread -lrt
+
+.PHONY:
+all:data.o mapped_data.o
+
+.PHONY:
+clean:
+	echo "Cleaning..."
+	rm -rf *.o
+
+%.o:%.cpp
+	g++ -lrt -Wall -ggdb -I /usr/local/boost_1_47_0 -I../zi_lib -I../zi_lib/external/include $< -o $@ 
+
+#Uncomment to debug Makefile
+#OLD_SHELL := $(SHELL)
+#SHELL = $(warning [$@ ($^) ($?)]) $(OLD_SHELL)
---
+++ mapped_data.cpp
@@ -0,0 +1,253 @@
+#include <boost/interprocess/file_mapping.hpp>
+#include <boost/interprocess/managed_mapped_file.hpp>
+#include <boost/interprocess/containers/map.hpp>
+#include <boost/interprocess/allocators/allocator.hpp>
+#include <iostream>
+#include <cstddef>
+#include <vector>
+#include <zi/hash.hpp>
+#include <zi/concurrency.hpp>
+#include <zi/utility/for_each.hpp>
+
+// get/set stuff
+using namespace boost::interprocess;
+
+template< typename T >
+struct storage_type
+{
+  std::size_t size;
+  T*          data;
+
+  storage_type( std::size_t s, T* d )
+    : size(s), data(d)
+  {}
+
+  storage_type()
+    : size(0), data(static_cast<T*>(0))
+  {}
+
+  storage_type( const storage_type& other )
+    : size(other.size), data(other.data)
+  {}
+
+};
+
+
+template< typename K, typename V >
+class storage_server
+{
+  
+private:
+    
+    
+  typedef std::pair<K, storage_type<V> > ValueType;
+  
+  //Alias an STL compatible allocator of for the map.
+  //This allocator will allow to place containers
+  //in managed shared memory segments
+  typedef allocator<ValueType, managed_mapped_file::segment_manager> 
+  MFileAllocator;
+
+  //Alias a map of ints that uses the previous STL-like allocator.
+  //Note that the third parameter argument is the ordering function
+  //of the map, just like with std::map, used to compare the keys.
+  typedef map<K, storage_type<V>, std::less<K>, MFileAllocator> FileMap;
+  typedef typename FileMap::iterator       data_iterator;
+  typedef typename FileMap::const_iterator data_const_iterator;
+
+  FileMap *mymap;
+  managed_mapped_file *mfile;
+  MFileAllocator *alloc_inst;
+  
+public:
+  storage_server(){
+    std::cout << "starting server...\n";
+    //Shared memory front-end that is able to construct objects
+    //associated with a c-string. Erase previous shared memory with the name
+    //to be used and create the memory segment at the specified address and initialize resources
+    mfile = new managed_mapped_file(open_or_create ,"MappedFile",4194304);
+
+    //Initialize the shared memory STL-compatible allocator
+    alloc_inst = new MFileAllocator(mfile->get_segment_manager());
+
+    //Construct a shared memory map.
+    //Note that the first parameter is the comparison function,
+    //and the second one the allocator.
+    //This the same signature as std::map's constructor taking an allocator
+    mymap = 
+      mfile->find_or_construct<FileMap>("FileMap")      //object name
+      (std::less<int>() //first  ctor parameter
+       ,*alloc_inst);     //second ctor parameter
+    std::cout << "server constructed\n";
+  }
+
+  ~storage_server(){
+    delete mfile;
+    delete alloc_inst;
+  }
+
+  // returns the data if its mapped, empty data otherwise
+  storage_type<V> get( const K& key ) const
+  {
+    data_const_iterator it = mymap->find(key);
+    if ( it != mymap->end() )
+      {
+	storage_type<V> x = it->second;
+	return it->second;
+      }
+    //return empty data on failure
+    return storage_type<V>();
+  }
+
+  // return whether it got replaced
+  bool set( const K& key, const storage_type<V>& store )
+  {
+    data_const_iterator it = mymap->find(key);
+    bool had_it = mymap->find(key) != mymap->end();
+    //*mymap[key] = store;
+    std::cout << "inserting key-value pair\n";
+    mymap->insert(ValueType(key,store));
+    std::cout << "inserted.\n";
+    return had_it;
+  }
+
+  bool set( const K& key, V* store, std::size_t size )
+  {
+    return set(key, storage_type<V>(size,store));
+  }
+};
+
+
+template< typename K, typename V, typename H = zi::hash<K> >
+class storage_client
+{
+private:
+  typedef storage_client<K,V,H> this_type;
+
+  static const std::size_t num_servers = 1;
+
+  // thrift connections, or just a list of computers... or whatever
+  // we (you) decide to have
+  storage_server<K,V> server_[num_servers];
+
+  H hasher_;
+
+  zi::task_manager::deque tm_;
+
+  void get_in_parallel( K key, storage_type<V>* dest,
+			std::size_t* num_left, zi::mutex* m, zi::condition_variable* cv )
+  {
+    *dest = get(key);
+    {
+      zi::mutex::guard g(*m);
+      --(*num_left);
+      if ( *num_left == 0 )
+	{
+	  cv->notify_one();
+	}
+      std::cout << "LEFT: " << *num_left << "\n";
+    }
+  }
+
+public:
+
+  storage_client()
+    : server_(), hasher_(), tm_(10)
+  {
+    std::cout << "starting client";
+    tm_.start();
+    std::cout << "client started";
+  }
+
+  ~storage_client()
+  {
+    tm_.join();
+  }
+
+  storage_type<V> get( const K& key ) const
+  {
+    std::cout << "Hashed to: " << (hasher_(key)%num_servers) << std::endl;
+    return server_[hasher_(key)%num_servers].get(key);
+  }
+
+  std::vector< std::pair<K,storage_type<V> > >
+  multi_get( const std::vector<K>& keys )
+  {
+    // this can also be done in parallel
+
+    std::vector< std::pair<K,storage_type<V> > > ret(keys.size());
+
+    std::size_t total_left = keys.size();
+    zi::mutex m;
+    zi::condition_variable cv;
+
+    std::size_t i = 0;
+    FOR_EACH( it, keys )
+      {
+	ret[i].first = *it;
+	tm_.push_back( zi::bind( &this_type::get_in_parallel, this, *it, &ret[i++].second,
+				 &total_left, &m, &cv ) );
+      }
+
+    {
+      zi::mutex::guard g(m);
+      while ( total_left > 0 )
+	{
+	  cv.wait(m);
+	}
+    }
+
+    return ret;
+  }
+
+  // return whether it got replaced
+  bool set( const K& key, const storage_type<V>& store )
+  {
+    return server_[hasher_(key)%num_servers].set(key,store);
+  }
+
+  bool set( const K& key, V* store, std::size_t size )
+  {
+    return set(key, storage_type<V>(size,store));
+  }
+
+};
+
+
+
+int main()
+{
+  storage_client<int,int> s;
+
+  int x[1];
+  int y[1];
+
+  x[0] = 1;
+  y[0] = 2;
+
+  std::cout << s.set(1,x,1) << "\n";
+  std::cout << s.set(1,x,1) << "\n";
+
+  std::cout << s.set(2,y,1) << "\n";
+  std::cout << s.set(2,y,1) << "\n";
+
+  std::cout << (s.get(2).data)[0] << "\n";
+  std::cout << (s.get(1).data)[0] << "\n";
+
+  std::vector<int> keys;
+  keys.push_back(1);
+  keys.push_back(2);
+  keys.push_back(1);
+  keys.push_back(2);
+  keys.push_back(1);
+  keys.push_back(2);
+
+  std::vector<std::pair<int,storage_type<int> > > r = s.multi_get(keys);
+
+  FOR_EACH( it, r )
+    {
+      std::cout << "Key: " << it->first << " Val: " << it->second.data[0] << "\n";
+    }
+  //remove mapped file
+  file_mapping::remove("MappedFile");
+}
