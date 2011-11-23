b6b4cc9ed86078e3abdb2846bc9c46a94a27315e
Matt Balkam <balkamm@gmail.com>
2011-11-23 09:58:23 -0500

    more cleanup, validation, and multithreaded thrift server

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,18 @@
 #include "serverHandlerNew.hpp"
 #include <protocol/TBinaryProtocol.h>
 #include <server/TSimpleServer.h>
+#include <server/TThreadPoolServer.h>
 #include <transport/TServerSocket.h>
 #include <transport/TBufferTransports.h>
+#include <concurrency/ThreadManager.h>
+#include <concurrency/PosixThreadFactory.h>
 #include <boost/shared_ptr.hpp>
 
 using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
 using namespace ::apache::thrift::transport;
 using namespace ::apache::thrift::server;
+using namespace ::apache::thrift::concurrency;
 
 using namespace ::om::server;
 
@@ -22,7 +26,14 @@ int main(int argc, char *argv[])
     boost::shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
     boost::shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
 
-    TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
+    boost::shared_ptr<ThreadManager> threadManager(ThreadManager::newSimpleThreadManager(32));
+    boost::shared_ptr<PosixThreadFactory> threadFactory(new PosixThreadFactory());
+
+    threadManager->threadFactory(threadFactory);
+    threadManager->start();
+
+    TThreadPoolServer server(processor, serverTransport, transportFactory,
+                             protocolFactory, threadManager);
     server.serve();
     return 0;
 }
--- src/serverHandlerNew.hpp
+++ src/serverHandlerNew.hpp
@@ -42,32 +42,19 @@ public:
     void get_chan_tile(server::tile& _return,
                        const server::metadata& vol,
                        const server::vector3d& point,
-                       const int32_t mipLevel,
                        const server::viewType::type view)
     {
+        validateMetadata(vol);
+
         coords::volumeSystem coordSystem(vol);
-
         coords::globalCoord coord = point;
-        coords::dataCoord dc = coord.toDataCoord(&coordSystem, mipLevel);
-        coords::chunkCoord cc = dc.toChunkCoord();
-//        std::cout << coord << " - " << dc << " - " << cc << " - "
-//                  << cc.chunkPtrOffset(&coordSystem, 4) << std::endl;
-        common::viewType viewType = common::Convert(view);
-        int depth = dc.toTileDepth(viewType);
+        coords::dataCoord dc = coord.toDataCoord(&coordSystem, vol.mipLevel);
 
-        if(!coordSystem.ContainsMipChunkCoord(cc)) {
-            throw common::argException("Requested data outside of volume.");
-        }
-
-        _return.view = view;
-
-        coords::dataBbox bounds = cc.chunkBoundingBox(&coordSystem);
-        _return.bounds.min = bounds.getMin().toGlobalCoord();
-        _return.bounds.max = bounds.getMax().toGlobalCoord();
+        setTileBounds(_return, dc, view);
 
         using namespace pipeline;
 
-        data_var encoded = mapData(vol.uri, vol.type) >> sliceTile(viewType, dc)
+        data_var encoded = mapData(vol.uri, vol.type) >> sliceTile(common::Convert(view), dc)
                                                       >> jpeg(128,128)
                                                       >> encode();
 
@@ -75,13 +62,14 @@ public:
         _return.data = std::string(out.data.get(), out.size);
     }
 
-    void get_seg_tiles(std::vector<tile> & _return,
+    void get_seg_tiles(std::map<std::string, tile> & _return,
                        const metadata& vol,
                        const int32_t segId,
                        const bbox& segBbox,
-                       const int32_t mipLevel,
                        const viewType::type view)
     {
+        validateMetadata(vol);
+
         coords::volumeSystem coordSystem(vol);
 
         pipeline::mapData dataSrc(vol.uri, vol.type);
@@ -96,15 +84,92 @@ public:
                 for(int z = min.z; z <= max.z; z += res.z) // always depth when twisted
                 {
                     coords::globalCoord coord = twist(coords::globalCoord(x,y,z), view);
-                    coords::dataCoord dc = coord.toDataCoord(&coordSystem, mipLevel);
-                    tile t = makeSegTile(dataSrc, dc, view, segId);
-                    _return.push_back(t);
+                    coords::dataCoord dc = coord.toDataCoord(&coordSystem, vol.mipLevel);
+
+                    tile t;
+                    makeSegTile(t, dataSrc, dc, view, segId);
+                    std::stringstream ss;
+                    ss << t.bounds.min.x << "-" << t.bounds.min.y << "-" << t.bounds.min.z;
+                    _return[ss.str()] = t;
                 }
             }
         }
     }
 
+    int32_t get_seg_id(const metadata& vol, const vector3d& point)
+    {
+        validateMetadata(vol);
+
+        coords::volumeSystem coordSystem(vol);
+
+        coords::globalCoord coord = point;
+        coords::dataCoord dc = coord.toDataCoord(&coordSystem, vol.mipLevel);
+
+        using namespace pipeline;
+
+        mapData dataSrc(vol.uri, vol.type);
+
+        data_var id = dataSrc >> getSegIds(dc, 0);
+
+        return boost::get<data<uint32_t> >(id).data.get()[0];
+    }
+
+    void get_seg_ids(std::vector<int32_t> & _return,
+                     const metadata& vol,
+                     const vector3d& point,
+                     const double radius,
+                     const viewType::type view)
+    {
+        validateMetadata(vol);
+
+        // Your implementation goes here
+        printf("get_seg_ids\n");
+    }
+
+    double compare_results(const std::vector<result> & old_results, const result& new_result)
+    {
+        // Your implementation goes here
+        printf("compare_results\n");
+        return 0;
+    }
+
 private:
+    void validateMetadata(const metadata& meta)
+    {
+        if(!file::exists(meta.uri)) {
+            throw common::argException("Cannot find requested volume.");
+        }
+
+        int factor = math::pow2int(meta.mipLevel);
+/*
+        std::size_t size = (meta.bounds.max.x - meta.bounds.min.x) *
+            (meta.bounds.max.y - meta.bounds.min.y) *
+            (meta.bounds.max.z - meta.bounds.min.z) /
+            (factor * factor * factor);
+
+        switch(meta.type)
+        {
+        case dataType::INT32:
+        case dataType::UINT32:
+        case dataType::FLOAT:
+            size *= 4;
+            break;
+        }
+
+        if(file::numBytes(meta.uri) != size) {
+            throw common::argException("Incorrect file size given bounds, mip level and data type.");
+        }
+*/
+
+        if(meta.resolution.x <= 0 || meta.resolution.y <= 0 || meta.resolution.z <= 0) {
+            throw common::argException("Resolution must be greater than 0.");
+        }
+
+        if(meta.chunkDims.x <= 0 || meta.chunkDims.y <= 0 || meta.chunkDims.z <= 0) {
+            throw common::argException("Chunk Dims must be greater than 0.");
+        }
+    }
+
     template<typename T>
     T twist(T vec, viewType::type view) const
     {
@@ -131,21 +196,34 @@ private:
         return out;
     }
 
-    tile makeSegTile(const pipeline::dataSrcs& src,
+    void makeSegTile(tile& t,
+                     const pipeline::dataSrcs& src,
                      const coords::dataCoord& dc,
                      const viewType::type& view,
                      uint32_t segId) const
     {
+        t.view = view;
+        setTileBounds(t, dc, view);
+
         using namespace pipeline;
 
+        data_var encoded = src >> sliceTile(common::Convert(view), dc)
+                               >> bitmask(segId)
+                               >> png(128,128)
+                               >> encode();
+
+        data<char> out = boost::get<data<char> >(encoded);
+        t.data = std::string(out.data.get(), out.size);
+    }
+
+    inline void setTileBounds(tile& t,
+                              const coords::dataCoord dc,
+                              const viewType::type& view) const
+    {
         coords::chunkCoord cc = dc.toChunkCoord();
-        common::viewType viewType = common::Convert(view);
-        int depth = dc.toTileDepth(viewType);
-
-        tile t;
-        t.view = view;
-
+        int depth = dc.toTileDepth(common::Convert(view));
         coords::dataBbox bounds = cc.chunkBoundingBox(dc.volume());
+
         vector3d min = twist(bounds.getMin().toGlobalCoord(), view);
         vector3d max = twist(bounds.getMax().toGlobalCoord(), view);
 
@@ -154,48 +232,6 @@ private:
 
         t.bounds.min = twist(min, view);
         t.bounds.max = twist(max, view);
-
-        data_var encoded = src >> sliceTile(viewType, dc) >> bitmask(segId)
-                               >> png(128,128) >> encode();
-
-        data<char> out = boost::get<data<char> >(encoded);
-        t.data = std::string(out.data.get(), out.size);
-
-        return t;
-    }
-
-public:
-    int32_t get_seg_id(const metadata& vol, const vector3d& point)
-    {
-        coords::volumeSystem coordSystem(vol);
-
-        coords::globalCoord coord = point;
-        coords::dataCoord dc = coord.toDataCoord(&coordSystem, 0);
-
-        using namespace pipeline;
-
-        mapData dataSrc(vol.uri, vol.type);
-
-        data_var id = dataSrc >> getSegIds(dc, 0);
-
-        return boost::get<data<uint32_t> >(id).data.get()[0];
-    }
-
-    void get_seg_ids(std::vector<int32_t> & _return,
-                     const metadata& vol,
-                     const vector3d& point,
-                     const double radius,
-                     const viewType::type view)
-    {
-        // Your implementation goes here
-        printf("get_seg_ids\n");
-    }
-
-    double compare_results(const std::vector<result> & old_results, const result& new_result)
-    {
-        // Your implementation goes here
-        printf("compare_results\n");
-        return 0;
     }
 };
 
