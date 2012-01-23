b16ec08b05039f7e81b03b7b2b37d4957263889f
Matt Balkam <balkamm@gmail.com>
2012-01-23 10:54:40 -0500

    Initial work on getting Segment Graphs.

---
+++ src/handler/getGraph.cpp
@@ -0,0 +1,17 @@
+#include "handler/handler.h"
+#include "pipeline/utility.hpp"
+#include "volume/volume.h"
+
+namespace om {
+namespace handler{
+
+void get_mst(std::vector<server::edge>& _return, const volume::volume& vol)
+{
+	
+}
+void get_graph(std::vector<server::edge>& _return, const volume::volume& vol)
+{
+	
+}
+
+}}		
