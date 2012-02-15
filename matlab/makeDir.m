9bcc8e8786902b0148372866021de71b675e87d2
Michael Purcaro <purcaro@gmail.com>
2012-02-15 03:01:17 -0500

    nuke more dead scripts, matlab, etc

--- matlab/makeDir.m
+++
@@ -1,5 +0,0 @@
-function makeDir( dir )
-    if ~exist( dir, 'dir' )
-       mkdir( dir );
-    end
-end
--- scripts/cluster/copyFile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/csh
-
-foreach node (`cat /home/purcaro/omni.staging/scripts/cluster/hosts | grep -v ^\#`)
-	echo "Syncing ${node}"
-	rsync --partial --progress /scratch/purcaro/Omni-data/E1088--justSegmentationNoMesh.omni ${node}:/scratch/purcaro/Omni-data/E1088--justSegmentationNoMesh.omni
-end
-
-# vim:ts=4:sw=4
