b6d8f39b70cfcbcd4ffc29d1af9929be9b319765
Matt Balkam <balkamm@gmail.com>
2012-09-27 11:11:33 -0400

    Added test for null volume in view2dstate.  Apparently it's only showing up in release version for 10.04...

--- desktop/src/view2d/omView2dState.hpp
+++ desktop/src/view2d/omView2dState.hpp
@@ -78,6 +78,10 @@ public:
         , segIDforPainting_(0)
         , coords_(vgs, viewType)
     {
+    	if(!vol) {
+    		throw OmArgException("vol cannot be null!");
+    	}
+
         coords_.setTotalViewport(size);
         zoomLevel_->Update(getMaxMipLevel());
 
