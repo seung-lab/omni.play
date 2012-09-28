cdcc9499bdcbff4ffd5962802d53dae162607855
Matt Balkam <balkamm@gmail.com>
2012-09-28 10:12:36 -0400

    Fixed bug with loading mangagers.

--- desktop/src/datalayer/archive/genericManager.hpp
+++ desktop/src/datalayer/archive/genericManager.hpp
@@ -17,14 +17,14 @@ public:
         out << YAML::Key << "valid set" << YAML::Value << gm.validSet_;
         out << YAML::Key << "enabled set" << YAML::Value << gm.enabledSet_;
         out << YAML::Key << "next id" << YAML::Value << gm.nextId_;
-        
+
         out << YAML::Key << "values" << YAML::Value << YAML::BeginSeq;
         FOR_EACH(iter, gm.validSet_){
             out << *gm.vec_[*iter];
         }
         out << YAML::EndSeq;
     }
-    
+
     template <class T>
     static void Load(const YAML::Node& in, OmGenericManager<T>& gm)
     {
@@ -32,15 +32,15 @@ public:
         in["valid set"] >> gm.validSet_;
         in["enabled set"] >> gm.enabledSet_;
         in["next id"] >> gm.nextId_;
-        
+
         gm.vec_.resize(gm.size_, NULL);
-        
+
         for(uint32_t i = 0; i < gm.validSet_.size(); ++i)
         {
             T* t = new T();
             in["values"][i] >> *t;
             gm.vec_[ t->GetID() ] = t;
-            
+
             gm.vecValidPtrs_.push_back(t);
         }
     }
