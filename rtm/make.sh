c08f829923ac357b035c2dc464cc2cfd0bcd726e
Matt Balkam <balkamm@gmail.com>
2013-02-25 16:00:12 -0500

    Fixed rtm make.sh

--- rtm/make.sh
+++ rtm/make.sh
@@ -1,2 +1,2 @@
 mkdir -p ../bin/release
-g++ -g src/im_server.cpp ../thrift/src/*.cpp -DHAVE_CONFIG_H -I../external/libs/boost/include -I../thrift/src -I. -I./src -I../external/libs/thrift/include/thrift -L../external/libs/thrift/lib -lrt -lpthread -lthrift -lz -lthriftnb -levent -DNDEBUG -O3  -o ../bin/release/rtmxz
+g++ -g src/im_server.cpp ../thrift/src/*.cpp -DHAVE_CONFIG_H -I../external/libs/boost/include -I../thrift/src -I. -I./src -I../external/libs/thrift/include/thrift -I../external/libs/thrift/include -L../external/libs/thrift/lib -lrt -lpthread -lthrift -lz -lthriftnb -levent -DNDEBUG -O3  -o ../bin/release/rtmxz
