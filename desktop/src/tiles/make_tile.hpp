24c757a4f1ce9c741c14d6d0ff1218ec5309c4f9
Michael Purcaro <purcaro@gmail.com>
2013-10-31 13:02:06 -0400

    small, simple tile pool--size constrained

--- desktop/src/tiles/make_tile.hpp
+++ desktop/src/tiles/make_tile.hpp
@@ -1,15 +1,63 @@
 #pragma once
 
+#include <zi/mutex.hpp>
 #include "utility/malloc.hpp"
 
 namespace om {
 namespace tile {
 
-// TODO: this could be converted into a tile pool
-//  (shared_ptr can take custom destructor....)
+template <class T, uint32_t NumEle, uint32_t NumTiles>
+class Cache {
+    std::array<std::array<T,NumEle>, NumTiles> cache_;
 
-template <class T> std::shared_ptr<T> Make() {
-  return om::mem::Malloc<T>::NumElementsDontZero(128 * 128);
+    zi::spinlock lock_;
+    std::vector<uint32_t> avail_;
+
+    std::pair<bool, uint32_t> getNextAvailIdx() {
+        zi::guard g(lock_);
+        if(avail_.empty()){
+            return {false, 0};
+        }
+        auto idx = avail_.back();
+        avail_.pop_back();
+        return {true, idx};
+    }
+
+public:
+    Cache(){
+        zi::guard g(lock_);
+        avail_.reserve(NumTiles);
+        for(uint32_t i = 0; i < NumTiles; ++i){
+            avail_.push_back(i);
+        }
+    }
+
+    std::shared_ptr<T> get(){
+        auto ifidx = getNextAvailIdx();
+        if(!ifidx.first){
+            return om::mem::Malloc<T>::NumElementsDontZero(NumEle);
+        }
+        auto idx = ifidx.second;
+        auto& arr = cache_[idx];
+        auto* ptr = &arr[0];
+
+        //std::cout << "allocating idx: " << idx << std::endl;
+
+        return std::shared_ptr<T>(ptr, [this,idx](T*){
+                this->done(idx);
+            });
+    }
+
+    void done(uint32_t idx){
+        zi::guard g(lock_);
+        avail_.push_back(idx);
+        //std::cout << "released idx: " << idx << std::endl;
+    }
+};
+
+template <class T> std::shared_ptr<T> Make(){
+    static Cache<T, 128*128, 1000> cache;
+    return cache.get();
 }
 
 }
