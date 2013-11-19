9171b7a8010e7988f098d05ace51b40bc7c90205
Shang Mu <smu@mit.edu>
2013-11-19 15:00:42 -0500

    Reimplement copySegmentsFromDefault() (as done on DataLayerRefactor branch) for creating user folders when the data didn't exist.

--- desktop/src/users/omUsersImpl.hpp
+++ desktop/src/users/omUsersImpl.hpp
@@ -16,18 +16,27 @@ class usersImpl {
   const QString usersFolderRoot_;
   QString userFolder_;
   std::unique_ptr<userSettings> settings_;
+  const std::string defaultUser_ = "_default";
 
  public:
   usersImpl() : usersFolderRoot_(usersFolderRoot()) {}
 
   void SwitchToDefaultUser() {
-    userFolder_ = QDir(usersFolderRoot_ + "_default").absolutePath();
+    userFolder_ = QDir(usersFolderRoot_ + QString::fromStdString(defaultUser_))
+                      .absolutePath();
     loadUserSettings();
   }
 
   void SwitchToUser(const std::string& userName) {
     userFolder_ = QDir(usersFolderRoot_ + QString::fromStdString(userName))
-        .absolutePath();
+                      .absolutePath();
+    log_variable(userFolder_.toStdString());
+
+    // assuming SegmentationID 1:
+    if (!om::file::exists(userFolder_.toStdString() +
+                          "/segmentations/segmentation1/segments")) {
+      copySegmentsFromDefault();
+    }
     loadUserSettings();
   }
 
@@ -48,7 +57,6 @@ class usersImpl {
   inline userSettings& UserSettings() { return *settings_; }
 
  private:
-
   QString usersFolderRoot() { return OmFileNames::FilesFolder() + "/users/"; }
 
   QString makeUserSegmentsFolder(const QString& folder) {
@@ -72,6 +80,9 @@ class usersImpl {
   }
 
   void fixSegmentFolderSymlink(const QString& folder) {
+    // Note this function and SetupFolders() are only called before a user is
+    // selected (the user is _default), although they may look like to be
+    // for any user.
     const QString oldSegmentsFolder = folder + "/segments";
     const QString userSegmentsFolder = makeUserSegmentsFolder(folder);
 
@@ -100,6 +111,40 @@ class usersImpl {
       std::cout << "Reloading User Settings...\n";
       settings_.reset(new userSettings(settingsFilename()));
       settings_->Load();
+    }
+  }
+
+  void copySegmentsFromDefault() {
+    try {
+      using std::string;
+      using namespace boost::filesystem;
+
+      string userFolder = userFolder_.toStdString();
+      string defaultUserFolder = usersFolderRoot_.toStdString() + defaultUser_;
+
+      string userSegments =
+          userFolder + "/segmentations/segmentation1/segments/";
+      string defaultUserSegments =
+          defaultUserFolder + "/segmentations/segmentation1/segments/";
+
+      om::file::MkDir(userFolder);
+      permissions(userFolder, all_all);
+      om::file::MkDir(userSegments);
+      permissions(userSegments, all_all);
+
+      om::file::CopyFile(defaultUserSegments + "mstUserEdges.data",
+                         userSegments + "mstUserEdges.data");
+      om::file::CopyFile(defaultUserSegments + "valid_group_num.data.ver1",
+                         userSegments + "valid_group_num.data.ver1");
+      permissions(userSegments + "mstUserEdges.data",
+                  all_all ^ owner_exe ^ group_exe ^ others_exe);
+      permissions(userSegments + "valid_group_num.data.ver1",
+                  all_all ^ owner_exe ^ group_exe ^ others_exe);
+    }
+    catch (const boost::filesystem::filesystem_error& e) {
+      log_errors(project / user) << "Error creating user segementation data: "
+                                 << e.what();
+      throw om::IoException(e.what());
     }
   }
 
