831fc5e963a7dedd85a47edf0d51f5cfe4779350
Matt Balkam <balkamm@gmail.com>
2013-11-25 17:52:11 -0500

    Added test for the correctness of the chunk UniqueValues values.

--- common/test/src/chunk/uniqueValuesDataSourceTest.cpp
+++ common/test/src/chunk/uniqueValuesDataSourceTest.cpp
@@ -4,6 +4,9 @@
 #include "utility/UUID.hpp"
 #include "chunk/uniqueValuesFileDataSource.hpp"
 #include "chunk/dataSources.hpp"
+#include "chunk/voxelGetter.hpp"
+#include "utility/volumeWalker.hpp"
+#include "volume/segmentation.h"
 
 using namespace om::chunk;
 
@@ -17,11 +20,30 @@ TEST(Chunk_UniqueValuesFileDataSource, Get) {
 
   std::shared_ptr<chunk::UniqueValues> values =
       uvfds.Get(coords::Chunk(0, 0, 0, 0));
-  ASSERT_TRUE((bool) values);
+  ASSERT_TRUE((bool)values);
   ASSERT_GT(values->Values.size(), 0);
 
   values = uvfds.Get(coords::Chunk(100, 0, 0, 0));
   ASSERT_FALSE(values);
+}
+
+TEST(Chunk_UniqueValuesFileDataSource, Completeness) {
+  volume::Segmentation s(URI);
+  log_variable(s.Coords().RootMipLevel());
+  Voxels<uint32_t> vg(s.ChunkDS(), s.Coords());
+  coords::Chunk cc(0, 1, 0, 1);
+
+  utility::VolumeWalker<uint32_t> walker(cc.BoundingBox(s.Coords()), vg);
+  std::set<uint32_t> voxels;
+  walker.foreach_voxel([&](coords::Data, uint32_t val) { voxels.insert(val); });
+
+  auto uv = s.UniqueValuesDS().Get(cc);
+
+  ASSERT_EQ(voxels.size(), uv->Values.size());
+
+  for (auto& v : voxels) {
+    ASSERT_TRUE(uv->contains(v));
+  }
 }
 
 TEST(Chunk_UniqueValuesFileDataSource, Put) {}
