6b0b64e3cd3a481f47bd29878428f67f7313f868
Matt Balkam <balkamm@gmail.com>
2014-01-23 16:57:22 -0500

    More testing for omni.server::get_connected_groups.

--- server/test/src/getConnectedGroups.cpp
+++ server/test/src/getConnectedGroups.cpp
@@ -11,6 +11,15 @@ using namespace ::zi::mesh;
 namespace om {
 namespace test {
 
+const server::group& getByType(const std::vector<server::group>& groups,
+                               om::server::groupType::type type) {
+  auto iter =
+      std::find_if(groups.begin(), groups.end(),
+                   [type](const server::group& g) { return g.type == type; });
+  EXPECT_TRUE(iter != groups.end());
+  return *iter;
+}
+
 TEST(GetConnectedGroupsTest, Simple) {
   file::Paths p("/omniData/e2198/e2198_a_s10_101_46_e17_116_61.omni");
   volume::Segmentation vol(p.Segmentation(1));
@@ -21,7 +30,22 @@ TEST(GetConnectedGroupsTest, Simple) {
 
   std::vector<server::group> _return;
   handler::get_connected_groups(_return, vol, groups);
-  EXPECT_EQ(_return.size(), 6);
+
+  EXPECT_EQ(6, _return.size());
+  auto all = getByType(_return, om::server::groupType::ALL);
+  EXPECT_EQ(1, all.groups.size());
+  EXPECT_EQ(7, all.groups.front().size());
+
+  auto agreed = getByType(_return, om::server::groupType::AGREED);
+  EXPECT_EQ(1, agreed.groups.size());
+  EXPECT_EQ(3, agreed.groups.front().size());
+
+  auto partial = getByType(_return, om::server::groupType::PARTIAL);
+  EXPECT_EQ(0, partial.groups.size());
+
+  auto dust = getByType(_return, om::server::groupType::DUST);
+  EXPECT_EQ(1, dust.groups.size());
+  EXPECT_EQ(4, dust.groups.front().size());
 }
 }
 }  // namespace om::test::
