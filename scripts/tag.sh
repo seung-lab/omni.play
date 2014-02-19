b9f1d5173d61100ff514f0969f4d49852cadac38
Matt Balkam <balkamm@gmail.com>
2014-02-19 14:38:09 -0500

    Clean up tagging script and update server minor version number.

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 project="$1"
+if [[ ! -d $project || ! -d $project/version/major || ! -d $project/version/minor ]]; then
+  echo "Invalid project"
+  exit(1);
+fi
+
 version_major=`cat $project/version/major`
 version_minor=`cat $project/version/minor`
 version=$version_major"."$version_minor
 tag=$project"-"$version
 
 git tag -a $tag -m ''
-git push origin --tags
 
-echo $version_minor+1 > $project/version/minor
+echo $((version_minor+1)) > $project/version/minor
+git add $project/version/minor
+git commit -m "Update minor version."
+git push origin --tags
