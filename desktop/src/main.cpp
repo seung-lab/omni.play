648ac2a75a6e2dd65643e6c69394412d9995e9f5
Matt Balkam <balkamm@gmail.com>
2014-02-27 15:57:30 -0500

    Quit when you set the version.

--- desktop/src/main.cpp
+++ desktop/src/main.cpp
@@ -123,6 +123,7 @@ int main(int argc, char *argv[]) {
 
   if (ZiARG_version) {
     std::cout << "Omni Desktop Version " << OMNI_DESKTOP_VERSION << std::endl;
+    return (0);
   }
 
   return Omni(argc, argv).Run();
