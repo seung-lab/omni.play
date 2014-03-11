a17216a03c91511f502522afdcb528a83c6f28e7
Shang Mu <smu@mit.edu>
2014-03-11 19:20:10 -0400

    Validate segment IDs before using them.

--- desktop/src/task/comparisonTask.cpp
+++ desktop/src/task/comparisonTask.cpp
@@ -52,7 +52,11 @@ bool ComparisonTask::Start() {
   if (allIter != namedGroups_.end()) {
     common::SegIDSet allRoots;
     for (const auto& id : allIter->segments) {
-      auto rootID = segments->FindRootID(id);
+      if (id <= 0 || id > segments->getMaxValue()) {
+        log_errors << "Invalid segment id " << id << " in 'All' segment group.";
+        continue;
+      }
+      auto rootID = segments->findRootID(id);
       if (rootID) {
         allRoots.insert(rootID);
       }
