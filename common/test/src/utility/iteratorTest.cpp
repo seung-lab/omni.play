d609e23d7f9150e60217c19b03e3ea51cdb46480
Matt Balkam <balkamm@gmail.com>
2014-04-15 16:00:04 -0400

    Made chunk iterator into a transform_iterator.

--- common/test/src/utility/iteratorTest.cpp
+++ common/test/src/utility/iteratorTest.cpp
@@ -23,5 +23,22 @@ TEST(Utility_Iterators, VectorIterator) {
             std::find(vector3_iterator<int>({1, 2, 3}, {2, 4, 6}),
                       vector3_iterator<int>(), val));
 }
+
+TEST(Utility_Iterators, ChunkIterator) {
+  size_t count = 0;
+
+  for (auto iter = make_chunk_iterator(1, {1, 2, 3}, {2, 4, 6});
+       iter != make_chunk_iterator(); ++iter) {
+    count++;
+    if (count > 30) {
+      break;
+    }
+  }
+  ASSERT_EQ(24, count);
+  coords::Chunk val(1, 1, 4, 3);
+  ASSERT_NE(make_chunk_iterator(),
+            std::find(make_chunk_iterator(1, {1, 2, 3}, {2, 4, 6}),
+                      make_chunk_iterator(), val));
+}
 }
 }  // namespace om::test::
