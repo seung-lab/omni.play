778a09e7f24064e8024aee78194d2bb96c19a718
wongwill86 <wongwill86@gmail.com>
2015-12-07 15:52:54 -0500

    Fixing centering omnification

--- desktop/src/volume/build/omProcessSegmentationChunk.hpp
+++ desktop/src/volume/build/omProcessSegmentationChunk.hpp
@@ -6,36 +6,43 @@
 
 class OmProcessSegmentationChunk {
  public:
-  OmProcessSegmentationChunk(const bool computeSizes, OmSegments& segments)
-      : computeSizes_(computeSizes), segments_(segments) {}
+  OmProcessSegmentationChunk(const bool computeSizes, const bool computeBounds, OmSegments& segments)
+      : computeSizes_(computeSizes), computeBounds_(computeBounds), segments_(segments) {}
 
   ~OmProcessSegmentationChunk() {
     for (auto& iter : cacheSegments_) {
       const om::common::SegID val = iter.first;
       OmSegment* seg = iter.second;
-      seg->addToSize(sizes_[val]);
-      seg->AddToBoundingBox(bounds_.at(val));
+      if (computeSizes_) {
+        seg->addToSize(sizes_[val]);
+      }
+      if (computeBounds_) {
+        seg->AddToBoundingBox(bounds_.at(val));
+      }
     }
   }
 
   inline void processVoxel(const om::common::SegID val,
                            const om::coords::Data& dc) {
     getOrAddSegment(val);
-    auto iter = bounds_.find(val);
-    if (iter == bounds_.end()) {
-      bounds_.emplace(val, om::coords::DataBbox(dc, dc));
-    } else {
-      iter->second.merge(om::coords::DataBbox(dc, dc));
+
+    if (computeBounds_) {
+      auto iter = bounds_.find(val);
+      if (iter == bounds_.end()) {
+        bounds_.emplace(val, om::coords::DataBbox(dc, dc));
+      } else {
+        iter->second.merge(om::coords::DataBbox(dc, dc));
+      }
     }
 
-    if (!computeSizes_) {
-      return;
+    if (computeSizes_) {
+      sizes_[val] = 1 + sizes_[val];
     }
-    sizes_[val] = 1 + sizes_[val];
   }
 
  private:
   const bool computeSizes_;
+  const bool computeBounds_;
   OmSegments& segments_;
 
   std::unordered_map<om::common::SegID, OmSegment*> cacheSegments_;
