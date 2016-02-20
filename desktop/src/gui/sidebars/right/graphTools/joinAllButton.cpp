73488a16ebffb36a302cb29da85edae8792b9b11
wongwill86 <wongwill86@gmail.com>
2016-02-19 20:44:14 -0500

    adding new joinall button (forgot to include in last commit)

---
+++ desktop/src/gui/sidebars/right/graphTools/joinAllButton.cpp
@@ -0,0 +1,30 @@
+#include "actions/omActions.h"
+#include "common/logging.h"
+#include "gui/sidebars/right/graphTools/graphTools.h"
+#include "gui/sidebars/right/graphTools/joinAllButton.h"
+#include "segment/omSegments.h"
+#include "segment/selection.hpp"
+#include "utility/dataWrappers.h"
+#include "volume/omSegmentation.h"
+
+JoinAllButton::JoinAllButton(GraphTools* d)
+    : OmButton<GraphTools>(d, "Join ALL", "Join all selected objects", false) {
+  /*
+   *om::connect(this, SIGNAL(clicked(bool)), this,
+   *            SLOT(enterOrExitJoinMode(bool)));
+   */
+}
+
+void JoinAllButton::doAction() {
+  SegmentationDataWrapper sdw = mParent->GetSDW();
+  if (!sdw.IsSegmentationValid()) {
+    return;
+  }
+
+  auto seg = sdw.GetSegmentation();
+  const om::common::SegIDSet ids =
+      seg->Segments().Selection().GetSelectedSegmentIDs();
+  OmActions::JoinSegments(sdw, ids);
+
+  mParent->updateGui();
+}
