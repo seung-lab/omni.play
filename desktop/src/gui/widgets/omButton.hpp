ab6d439bcaa7229f5e42dd724ff103f37c399a98
wongwill86 <wongwill86@gmail.com>
2016-03-17 23:33:00 -0400

    Allow button declarations using lambdas as well as inheritance

--- desktop/src/gui/widgets/omButton.hpp
+++ desktop/src/gui/widgets/omButton.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "precomp.h"
 #include "system/omConnect.hpp"
-
+#include "functional"
 
 class OmButton : public QPushButton {
 Q_OBJECT
@@ -11,18 +11,24 @@ Q_OBJECT
    * We should set left/right/toggle lambdas on construction!
    */
   OmButton(QWidget* mw, const QString& title, const QString& statusTip,
-           const bool isCheckable)
-      : QPushButton(mw), mParent(mw) {
+           const bool isCheckable,
+           std::function<void(bool)> onLeftClickLambda = {},
+           std::function<void(bool)> onRightClickLambda = {},
+           std::function<void(bool)> onToggleLambda = {})
+      : QPushButton(mw), mParent(mw),
+        onLeftClickLambda_(onLeftClickLambda),
+        onRightClickLambda_(onRightClickLambda),
+        onToggleLambda_(onToggleLambda) {
     setText(title);
     setStatusTip(statusTip);
     setCheckable(isCheckable);
 
-    om::connect(this, SIGNAL(clicked()), this,
-        SLOT(callLeftClickAction()));
-    om::connect(this, SIGNAL(rightClicked()), this,
-        SLOT(callRightClickAction()));
+    om::connect(this, SIGNAL(clicked(bool)), this,
+        SLOT(callLeftClick(bool)));
+    om::connect(this, SIGNAL(rightClicked(bool)), this,
+        SLOT(callRightClick(bool)));
     om::connect(this, SIGNAL(toggled(bool)), this,
-        SLOT(callToggleAction(bool)));
+        SLOT(callToggle(bool)));
   }
 
   void setKeyboardShortcut(const QString& shortcut) {
@@ -46,6 +52,10 @@ Q_OBJECT
   virtual void onRightClick() {}
   virtual void onToggle(bool isChecked) {}
 
+  const std::function<void(bool)> onLeftClickLambda_;
+  const std::function<void(bool)> onRightClickLambda_;
+  const std::function<void(bool)> onToggleLambda_;
+
   // Patch in signal for right clicking
   void mousePressEvent(QMouseEvent* event) {
     QPushButton::mousePressEvent(event);
@@ -55,14 +65,35 @@ Q_OBJECT
     }
     // A bit different from Qt src impl, we don't have have Q_D or Q_Q 
     // to call the signal, so we manually call the signal
-    rightClicked();
+    rightClicked(isChecked());
+  }
+
+
+ protected Q_SLOTS:
+  void callLeftClick(bool isChecked) {
+    if(onLeftClickLambda_) {
+      onLeftClickLambda_(isChecked);
+    } else {
+      onLeftClick();
+    }
+  }
+
+  void callRightClick(bool isChecked) {
+    if(onRightClickLambda_) {
+      onRightClickLambda_(isChecked);
+    } else {
+      onRightClick();
+    }
+  }
+
+  void callToggle(bool isChecked) {
+    if(onToggleLambda_) {
+      onToggleLambda_(isChecked);
+    } else {
+      onToggle(isChecked);
+    }
   }
 
 Q_SIGNALS:
-  void rightClicked();
-
- protected Q_SLOTS:
-  virtual void callLeftClickAction() { onLeftClick(); }
-  virtual void callRightClickAction() { onRightClick(); }
-  virtual void callToggleAction(bool isChecked) { onToggle(isChecked); }
+  void rightClicked(bool isChecked);
 };
