726791e0c85189e9095b518ecaf6a5a22b3057ec
wongwill86 <wongwill86@gmail.com>
2016-03-31 16:47:32 -0400

    Refactored out typedefs preparing segment data into graph

--- desktop/src/segment/minCut.hpp
+++ desktop/src/segment/minCut.hpp
@@ -4,31 +4,28 @@
 #include "segment/types.hpp"
 #include "common/logging.h"
 #include "utility/segmentationDataWrapper.hpp"
+#include "utility/boostSegmentGraph.hpp"
 #include "segment/omSegment.h"
 #include "segment/omSegments.h"
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/boykov_kolmogorov_max_flow.hpp>
-#include <boost/graph/read_dimacs.hpp>
-#include <boost/graph/graph_utility.hpp>
+#include <boost/graph/properties.hpp>
+
+using namespace om::segment::boostgraph;
 
 class MinCut {
 
-  typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> Traits;
-  typedef typename Traits::vertex_descriptor Vertex;
-  typedef typename Traits::edge_descriptor Edge;
-  typedef boost::adjacency_list < boost::vecS, boost::vecS, boost::directedS,
-            boost::property <boost::vertex_name_t, std::string,
-            boost::property <boost::vertex_index_t, long,
-            boost::property <boost::vertex_color_t, boost::default_color_type,
-            boost::property <boost::vertex_distance_t, long,
-            boost::property <boost::vertex_predecessor_t, Edge>>>>>,
-            boost::property <boost::edge_capacity_t, long,
-            boost::property <boost::edge_residual_capacity_t, long,
-            boost::property <boost::edge_reverse_t, Edge>>>> Graph;
-
+  Color SOURCE = DefaultColors::black();
+  Color SINK = DefaultColors::white();
 
  public:
-  MinCut(OmSegments& segments) : segments_(segments) {}
+  MinCut(OmSegments& segments)
+    : segments_(segments), graph_(Graph()),
+      capacityProperty_(boost::get(boost::edge_capacity, graph_)),
+      colorProperty_(boost::get(boost::vertex_color, graph_)),
+      nameProperty_(boost::get(boost::vertex_name, graph_)),
+      residualCapacityProperty_(boost::get(boost::edge_residual_capacity, graph_)),
+      reverseProperty_(boost::get(boost::edge_reverse, graph_)) {}
 
   om::segment::UserEdge findEdge(om::common::SegIDSet sources,
       om::common::SegIDSet sinks) {
@@ -48,70 +45,78 @@ class MinCut {
     return om::segment::UserEdge();
   }
 
-  Graph createGraph() {
-    Graph g;
-    boost::property_map<Graph, boost::edge_capacity_t>::type
-      capacity = boost::get(boost::edge_capacity, g);
-    boost::property_map<Graph, boost::edge_residual_capacity_t>::type
-      residual_capacity = boost::get(boost::edge_residual_capacity, g);
-    boost::property_map<Graph , boost::edge_reverse_t>::type
-      rev = boost::get(boost::edge_reverse, g);
-    boost::property_map<Graph, boost::vertex_name_t>::type
-      name = boost::get(boost::vertex_name, g);
-    boost::property_map<Graph, boost::vertex_color_t>::type
-      color = boost::get(boost::vertex_color, g);
+  void addEdge(Vertex& vertex1, Vertex& vertex2, int threshold) {
+      Edge edgeForward, edgeReverse;
+      bool isForwardCreated, isReverseCreated;
+      boost::tie(edgeForward, isForwardCreated) =
+        boost::add_edge(vertex1, vertex2, graph_);
+      boost::tie(edgeReverse, isReverseCreated) =
+        boost::add_edge(vertex2, vertex1, graph_);
+      if (!isForwardCreated || !isReverseCreated) {
+        log_errors << "Unable to create edge correctly between " << vertex1 <<
+                  " and " << vertex2 << std::endl;
+        return;
+      }
+      capacityProperty_[edgeForward] = threshold;
+      capacityProperty_[edgeReverse] = threshold;
+      reverseProperty_[edgeForward] = edgeReverse;
+      reverseProperty_[edgeReverse] = edgeForward;
+  }
 
+  void populateGraph() {
     std::vector<Vertex> vertices;
     for (int i = 0; i < 10; ++i) {
-      Vertex v = boost::add_vertex(g);
+      Vertex v = boost::add_vertex(graph_);
       vertices.push_back(v);
-      boost::put(name, v, std::to_string(i));
+      boost::put(nameProperty_, v, std::to_string(i));
     }
 
     for (int i = 1; i < 10; ++i) {
-      Edge e1, e2;
-      bool in1, in2;
-      boost::tie(e1, in1) = boost::add_edge(vertices[i-1], vertices[i], g);
-      boost::tie(e2, in2) = boost::add_edge(vertices[i], vertices[i-1], g);
-      if (!in1 || !in2) {
-        continue;
-      }
+      int cap;
       if (i == 5) {
-        capacity[e1] = 1;
+        cap = 1;
       } else {
-        capacity[e1] = 2;
+        cap = 2;
       }
-      capacity[e2] = 0;
-      rev[e1] = e2;
-      rev[e2] = e1;
+      addEdge(vertices[i], vertices[i-1], cap);
     }
     Vertex s, t;
 
     s = *vertices.begin();
     t = *(vertices.end() - 1);
-    //std::vector<boost::default_color_type> color(boost::num_vertices(g));
-    //std::vector<long> distance(boost::num_vertices(g));
-    std::cout << "running boykov from " << name[s] << " to " << name[t] << std::endl;
-    long flow = boost::boykov_kolmogorov_max_flow(g, s, t);
+    //std::vector<boost::default_color_type> color(boost::num_vertices(graph_));
+    //std::vector<long> distance(boost::num_vertices(graph_));
+    std::cout << "running boykov from " << nameProperty_[s] << " to " << nameProperty_[t] << std::endl;
+    long flow = boost::boykov_kolmogorov_max_flow(graph_, s, t);
+  }
 
+  std::vector<Edge> getMinCutEdges() {
     boost::template graph_traits<Graph>::vertex_iterator u_iter, u_end;
     boost::template graph_traits<Graph>::out_edge_iterator ei, e_end;
 
-    for (boost::tie(u_iter, u_end) = boost::vertices(g); u_iter != u_end; ++u_iter) {
-      for (boost::tie(ei, e_end) = boost::out_edges(*u_iter, g); ei != e_end; ++ei) {
-        Vertex target = boost::target(*ei, g);
-        if (capacity[*ei] > 0) {
-          std::cout << "f " << *u_iter << " (name = " << name[*u_iter] <<
-            "color = " << color[*u_iter] << ") to " << target << " (name = " << name[target] <<
-            "color = " << color[target] << ") - " <<(capacity[*ei] - residual_capacity[*ei]) << std::endl;
+    std::vector<Edge> minEdges;
+    for (boost::tie(u_iter, u_end) = boost::vertices(graph_); u_iter != u_end; ++u_iter) {
+      for (boost::tie(ei, e_end) = boost::out_edges(*u_iter, graph_); ei != e_end; ++ei) {
+        Vertex target = boost::target(*ei, graph_);
+        if (capacityProperty_[*ei] > 0) {
+          std::cout << "f " << *u_iter << " (name = " << nameProperty_[*u_iter] <<
+            "color = " << colorProperty_[*u_iter] << ") to " << target <<
+            " (name = " << nameProperty_[target] <<
+            "color = " << colorProperty_[target] << ") - " <<
+            (capacityProperty_[*ei] - residualCapacityProperty_[*ei]) << std::endl;
         }
       }
     }
-    return g;
   }
 
  private:
   OmSegments& segments_;
+  Graph graph_;
+  CapacityProperty capacityProperty_;
+  ColorProperty colorProperty_;
+  NameProperty nameProperty_;
+  ResidualCapacityProperty residualCapacityProperty_;
+  ReverseProperty reverseProperty_;
 
   bool hasRoot(om::common::SegIDSet segIDSet, OmSegment* desiredRoot) {
     for (auto segID : segIDSet) {
--- desktop/test/src/segment/minCutTest.cpp
+++ desktop/test/src/segment/minCutTest.cpp
@@ -6,16 +6,29 @@
 #include "segment/omSegmentsImpl.h"
 #include "volume/omSegmentation.h"
 #include "project/omProject.h"
+#include "utility/boostSegmentGraph.hpp"
 
 const std::string fnp("../../test_data/build_project/meshTest.omni");
 
-std::vector<om::segment::Data> prepareData() {
+namespace test {
+namespace minCut {
+
+om::coords::VolumeSystem VOLUME_SYSTEM;
+om::common::SegListType SEG_LIST_TYPE = om::common::SegListType::WORKING;
+
+std::vector<om::segment::Data> prepareSegmentData(int numSegments) {
   std::vector<om::segment::Data> data;
-  for (uint32_t i = 0; i < 5; i++) {
+  for (uint32_t i = 0; i < numSegments; i++) {
     data.push_back({.value = i});
   }
   return data;
 }
+
+std::unique_ptr<OmSegment> createSegment(std::vector<om::segment::Data> data,
+    om::common::SegID segID) {
+  return std::make_unique<OmSegment>(data[segID], SEG_LIST_TYPE, VOLUME_SYSTEM);
+}
+void addEdge();
 
 class MockSegmentsImpl : public OmSegmentsImpl {
 public:
@@ -50,13 +63,11 @@ TEST(minCut, testNotSameRoot) {
 
   // prepare test data
   // segmentds with id 3 and 4 will have roots 1 and 2, respectively.
-  std::vector<om::segment::Data> data = prepareData();
-  om::coords::VolumeSystem volumeSystem;
-  om::common::SegListType segListType = om::common::SegListType::WORKING;
+  std::vector<om::segment::Data> data = prepareSegmentData(5);
   std::unique_ptr<OmSegment> rootSegment1(
-      new OmSegment(data[1], segListType, volumeSystem));
+      new OmSegment(data[1], SEG_LIST_TYPE, VOLUME_SYSTEM));
   std::unique_ptr<OmSegment> rootSegment2(
-      new OmSegment(data[2], segListType, volumeSystem));
+      new OmSegment(data[2], SEG_LIST_TYPE, VOLUME_SYSTEM));
   EXPECT_CALL(static_cast<MockSegmentsImpl&>(*mockSegmentsImpl), FindRoot(3))
     .WillRepeatedly(testing::Return(rootSegment1.get()));
   EXPECT_CALL(static_cast<MockSegmentsImpl&>(*mockSegmentsImpl), FindRoot(4))
@@ -79,43 +90,8 @@ TEST(minCut, testNotSameRoot) {
   EXPECT_FALSE(returnEdge.valid);
 }
 
-TEST(minCut, testGraph) {
-  // necessary setup for OmSegments
-  OmProject::New(QString::fromStdString(fnp));
-  OmSegmentation* segmentation = &SegmentationDataWrapper().Create();
-
-  // set up MockSegmentsImpl and data
-  std::unique_ptr<OmSegmentsImpl> 
-    mockSegmentsImpl(new MockSegmentsImpl(segmentation));
-
-  // prepare test data
-  // segmentds with id 3 and 4 will have roots 1 and 2, respectively.
-  std::vector<om::segment::Data> data = prepareData();
-  om::coords::VolumeSystem volumeSystem;
-  om::common::SegListType segListType = om::common::SegListType::WORKING;
-  std::unique_ptr<OmSegment> rootSegment1(
-      new OmSegment(data[1], segListType, volumeSystem));
-  std::unique_ptr<OmSegment> rootSegment2(
-      new OmSegment(data[2], segListType, volumeSystem));
-  EXPECT_CALL(static_cast<MockSegmentsImpl&>(*mockSegmentsImpl), FindRoot(3))
-    .WillRepeatedly(testing::Return(rootSegment1.get()));
-  EXPECT_CALL(static_cast<MockSegmentsImpl&>(*mockSegmentsImpl), FindRoot(4))
-    .WillRepeatedly(testing::Return(rootSegment2.get()));
-
-  OmSegments mockSegments(segmentation, std::move(mockSegmentsImpl));
-  // mockSegmentsImpl is no longer valid after move!
-
-  // prepare test inputs
-  om::common::SegIDSet sources;
-  om::common::SegIDSet sinks;
-  sources.insert(3);
-  sinks.insert(4);
-  MinCut minCut(mockSegments);
-
-  // test
-  //om::segment::UserEdge returnEdge = minCut.findEdge(sources, sinks);
-  minCut.createGraph();
-
-  // verify
-  //EXPECT_FALSE(returnEdge.valid);
+TEST(minCut, findMinCutBoostGraph) {
 }
+
+} //namespace mincut
+} //namespace test
